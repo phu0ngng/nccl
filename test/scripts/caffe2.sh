d05931e02b1067a7e0f5d9760f2053b60505590f
Anshuman Goswami <agoswami@nvidia.com>
2017-10-25 16:46:53 -0700

    Updated script and notes for C2, CNTK and PyTorch

--- test/scripts/caffe2.sh
+++ test/scripts/caffe2.sh
@@ -16,6 +16,6 @@ if [ "$gpumodel" == "dgx1v" ]; then
 fi
 
 NCCL_DISABLE_CHECKS=1 NCCL_DEBUG=WARN srun -p $gpumodel --exclusive \
-python $CAFFE2_ROOT/nvidia-examples/imagenet/train_resnet.py --train-lmdb /data/imagenet/train-lmdb-256x256 --skip-test --batch-size 512 --num-iterations 500 $extra --all-gpus --epochs 1 | \
+python $CAFFE2_ROOT/nvidia-examples/imagenet/train_resnet.py --train-lmdb /data/imagenet/train-lmdb-256x256 --skip-test --batch-size 512 --num-iterations 1000 $extra --all-gpus --dtype float16 | \
 tee /dev/stderr | awk -F'=' '/Epoch/ {if (lines++) sum += $NF} END {print sum/(lines-1)}' | \
 tee -a $result.out
--- test/scripts/nccl_test_node.sh
+++ test/scripts/nccl_test_node.sh
@@ -45,14 +45,12 @@ fi
 
 if [ "$mode" == "dlfw" ] && [ "$gpumodel" == "P100" ]; then
   cd $BLDDIR
-  if [ "$opt" == "PARALLEL" ]; then
-    echo "dlfw GROUP/PARALLEL $opt"
-    #$SHDIR/caffe2.sh $gpumodel
-    #$SHDIR/tensorflow.sh $gpumodel
-  fi
-  #$SHDIR/cntk.sh $gpumodel $mode
+  export LD_LIBRARY_PATH=$BLDDIR/lib:$LD_LIBRARY_PATH
+  $SHDIR/caffe2.sh $gpumodel $mode
+  $SHDIR/cntk.sh $gpumodel $mode
+  #$SHDIR/tensorflow.sh $gpumodel $mode
   #$SHDIR/mxnet.sh $gpumodel $mode
-  #$SHDIR/pytorch.sh $gpumodel $mode
+  $SHDIR/pytorch.sh $gpumodel $mode
 elif [[ "$mode" == *"mpi"* ]] || [[ "$mode" == *"multinode"* ]]; then
   # test (multi processes)
   cd $NCCLROOT
