9d48fcbc3722d1102a447f2f1f7adc777ef559c8
Ke Wen <kwen@nvidia.com>
2017-11-09 16:13:33 -0800

    test/scripts: added 2.0.5 FW results for tracking regression

--- test/scripts/generate_all.sh
+++ test/scripts/generate_all.sh
@@ -66,6 +66,6 @@ done
 DIR=html_dlfw
 rm -rf $DST/$DIR
 mkdir -p $DST/$DIR
-./dlfw_generate_plots.sh P100 ${VER}${GM} ${VER}${PM}
+./dlfw_generate_plots.sh P100 ${VER}${GM} ${VER}${PM} 2.0.5
 mv comp/P100/*.png $DST/$DIR/
 rm -rf comp
