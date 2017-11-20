8213b4a7a8388e80c4c7ebb3a518ff435494e3cf
Sylvain Jeaugey <sjeaugey@nvidia.com>
2017-11-20 12:16:00 -0800

    IB selection clean-up

--- src/transport/net_ib.cu
+++ src/transport/net_ib.cu
@@ -411,10 +411,14 @@ int ncclIbConnect(int dev, void* opaqueHandle, void** sendComm) {
   struct ibv_port_attr portAttr;
   NCCLCHECK(wrap_ibv_query_port(ctx, ib_port, &portAttr));
   struct ncclIbQpInfo qpInfo;
-  qpInfo.lid = portAttr.lid;
   qpInfo.ib_port = ib_port;
   qpInfo.qpn = comm->qp->qp_num;
   qpInfo.mtu = portAttr.active_mtu;
+
+  // Prepare my fifo
+  NCCLCHECK(wrap_ibv_reg_mr(&comm->fifoMr, comm->verbs.pd, comm->fifo, sizeof(struct ncclIbSendFifo)*MAX_REQUESTS, IBV_ACCESS_LOCAL_WRITE|IBV_ACCESS_REMOTE_WRITE|IBV_ACCESS_REMOTE_READ));
+  qpInfo.fifoRkey = comm->fifoMr->rkey;
+  qpInfo.fifoAddr = (uint64_t)comm->fifo;
 
   // RoCE support
   static int ibGidIndex = -1;
@@ -422,22 +426,18 @@ int ncclIbConnect(int dev, void* opaqueHandle, void** sendComm) {
     char* str = getenv("NCCL_IB_GID_INDEX");
     ibGidIndex = str ? atoi(str) : 0;
   }
-  static union ibv_gid gid;
-  NCCLCHECK(wrap_ibv_query_gid(ctx, ib_port, ibGidIndex, &gid));
-  qpInfo.spn = gid.global.subnet_prefix;
-  qpInfo.iid = gid.global.interface_id;
 
-  // Prepare my fifo
-  NCCLCHECK(wrap_ibv_reg_mr(&comm->fifoMr, comm->verbs.pd, comm->fifo, sizeof(struct ncclIbSendFifo)*MAX_REQUESTS, IBV_ACCESS_LOCAL_WRITE|IBV_ACCESS_REMOTE_WRITE|IBV_ACCESS_REMOTE_READ));
-  qpInfo.fifoRkey = comm->fifoMr->rkey;
-  qpInfo.fifoAddr = (uint64_t)comm->fifo;
-
-  if (qpInfo.lid) {
+  qpInfo.lid = portAttr.lid;
+  if (qpInfo.lid) { // IB
     INFO("NET/IB: Dev %d Port %d qpn %d mtu %d LID %d", dev, ib_port, qpInfo.qpn, qpInfo.mtu, qpInfo.lid);
-  } else {
+  } else { // RoCE
+    union ibv_gid gid;
+    NCCLCHECK(wrap_ibv_query_gid(ctx, ib_port, ibGidIndex, &gid));
+    qpInfo.spn = gid.global.subnet_prefix;
+    qpInfo.iid = gid.global.interface_id;
     INFO("NET/IB: Dev %d Port %d qpn %d mtu %d GID %d (%lX/%lX)", dev, ib_port, qpInfo.qpn, qpInfo.mtu, ibGidIndex, qpInfo.spn, qpInfo.iid);
   }
-   
+
   NCCLCHECK(socketSend(comm->fd, &qpInfo, sizeof(qpInfo)));
   return 0;
 }
