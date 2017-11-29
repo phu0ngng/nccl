487e24eb901fc98bf5db948e1938feba6b4d76c8
Sylvain Jeaugey <sjeaugey@nvidia.com>
2017-11-28 17:48:05 -0800

    Add PLC templates and scripts

---
+++ plc/create_new.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "Usage : $0 Title of the feature"
+  exit 1
+fi
+
+title="$@"
+
+item=1
+while [ -d items/$item ]; do item=`expr $item + 1`; done
+
+mkdir items/$item
+for template in templates/*; do
+  sed -e "s/\${plc:item}/$item/g" \
+      -e "s/\${plc:title}/$title/g" \
+  $template > items/$item/`basename $template`
+done
+
+echo "Created item $item ($PWD/items/$item)"
