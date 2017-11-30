e5a83ba1d10c2e07d716889e7ea81a67de6ce3e4
Ke Wen <kwen@nvidia.com>
2017-11-29 17:02:55 -0800

    test/perf: add warm-up for small-size launch kernel

--- test/perf/common.cu
+++ test/perf/common.cu
@@ -611,8 +611,12 @@ void setupArgs(size_t size, ncclDataType_t type, struct threadArgs_t* args) {
 }
 
 void TimeTest(struct threadArgs_t* args, ncclDataType_t type, const char* typeName, ncclRedOp_t op, const char* opName, int root, int inPlace) {
-  // Warm-up
+  // Warm-up for large size
   setupArgs(args->maxbytes, type, args);
+  BenchTime(args, type, op, root, 0, 1);
+
+  // Warm-up for small size
+  setupArgs(args->minbytes, type, args);
   BenchTime(args, type, op, root, 0, 1);
 
   // Benchmark
