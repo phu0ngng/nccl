3ef446a23aab0db7a5bb04b8251aa6b1e1c2ea7c
Sylvain Jeaugey <sjeaugey@nvidia.com>
2017-12-05 17:15:20 -0800

    Add table of contents

--- plc/html/tools/index_titles.py
+++ plc/html/tools/index_titles.py
@@ -5,14 +5,16 @@ import os,sys
 h2=0
 h3=0
 h4=0
-contents=""
+contents="<div class=contents><h2>Table of contents</h2>"
+
+lines = []
 
 for line in open(sys.argv[1]).readlines() :
-  if "<h1>" in line :
-    label = "top"
-    title = (line.split("<h1>")[1]).split("</h1>")[0]
-    line = line.replace("<h1>", "<a name=\"%s\"></a><h1>" % label)
-    contents += "<p><b><a href=\"#%s\">%s</a></b><p>\n" % (label, title)
+#  if "<h1>" in line :
+#    label = "top"
+#    title = (line.split("<h1>")[1]).split("</h1>")[0]
+#    line = line.replace("<h1>", "<a name=\"%s\"></a><h1>" % label)
+#    contents += "<p><b><a href=\"#%s\">%s</a></b><p>\n" % (label, title)
   if "<h2>" in line :
     h2+=1
     h3=1
@@ -20,7 +22,7 @@ for line in open(sys.argv[1]).readlines() :
     label = "%d" % h2;
     title = (line.split("<h2>")[1]).split("</h2>")[0]
     line = line.replace("<h2>", "<a name=\"%s\"></a><h2>%s " % (label, label))
-    contents += "<hr><a href=\"#%s\">%s %s</a><br>\n" % (label, label, title)
+    contents += "<a href=\"#%s\">%s %s</a><br>\n" % (label, label, title)
   if "<h3>" in line :
     h3+=1
     h4=1
@@ -34,6 +36,11 @@ for line in open(sys.argv[1]).readlines() :
     title = (line.split("<h4>")[1]).split("</h4>")[0]
     line = line.replace("<h4>", "<a name=\"%s\"></a><h4>%s " % (label,label))
     contents += "&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"#%s\">%s %s</a><br>\n" % (label, label, title)
+  lines.append(line)
+
+contents += "</div>"
+
+for line in lines :
   if "$CONTENTS" in line :
     line = line.replace("$CONTENTS", contents)
   print line,
