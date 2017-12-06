00c23cf9aef3a43c37a978c7fbe669abb3895661
Sylvain Jeaugey <sjeaugey@nvidia.com>
2017-12-06 10:32:58 -0800

    PLC : Move most of the makefile into common.mk

--- plc/items/common/common.mk
+++ plc/items/common/common.mk
@@ -1,3 +1,54 @@
-BUILDDIR=.
+BUILDDIR?=.
 
-CHROME=chromium-browser
+CHROME?=chromium-browser
+
+REQ=NCCL-Req-$(ITEM)
+DESIGN=NCCL-Design-$(ITEM)
+CODING=NCCL-Coding-$(ITEM)
+TESTING=NCCL-Testing-$(ITEM)
+HTML=$(BUILDDIR)/index.html $(BUILDDIR)/$(REQ).html $(BUILDDIR)/$(DESIGN).html $(BUILDDIR)/$(CODING).html $(BUILDDIR)/$(TESTING).html
+PDF=$(BUILDDIR)/$(REQ).pdf $(BUILDDIR)/$(DESIGN).pdf $(BUILDDIR)/$(CODING).pdf $(BUILDDIR)/$(TESTING).pdf
+
+all: $(PDF) $(HTML)
+pdf: $(PDF)
+html: $(HTML)
+
+$(BUILDDIR)/%.pdf: $(BUILDDIR)/%.html
+	mkdir -p $(BUILDDIR)
+	$(CHROME) --headless --print-to-pdf="$@" file://$<
+	chmod 644 $@
+
+$(BUILDDIR)/index.html: index.html
+	cp $< $@
+
+$(BUILDDIR)/$(REQ).html: req.html
+	mkdir -p $(BUILDDIR)
+	cp $< $@.tmp
+	rsync -av images $(BUILDDIR)
+	../../html/tools/git_history.sh $< >> $@.tmp
+	../../html/tools/index_titles.py $@.tmp > $@
+	rm $@.tmp
+
+$(BUILDDIR)/$(DESIGN).html: design.html
+	mkdir -p $(BUILDDIR)
+	cp $< $@.tmp
+	rsync -av images $(BUILDDIR)
+	../../html/tools/git_history.sh $< >> $@.tmp
+	../../html/tools/index_titles.py $@.tmp > $@
+	rm $@.tmp
+
+$(BUILDDIR)/$(TESTING).html: testing.html
+	mkdir -p $(BUILDDIR)
+	cp $< $@.tmp
+	rsync -av images $(BUILDDIR)
+	../../html/tools/git_history.sh $< >> $@.tmp
+	../../html/tools/index_titles.py $@.tmp > $@
+	rm $@.tmp
+
+$(BUILDDIR)/$(CODING).html: coding.html coding.list
+	mkdir -p $(BUILDDIR)
+	cp $< $@
+	../../html/tools/git_to_html.sh coding.list >> $@
+
+clean:
+	rm -f $(HTML) $(PDF)
