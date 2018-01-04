43aa52c77747935312d5728cd7bac3017defdd81
David Addison <daddison@nvidia.com>
2018-01-04 13:52:46 -0800

    Fix for bug NvBug 2031942/JIRA NCCL-402
    Fix GROUP mode SEGV when a node has mixed V100 and Titan X Pascal

--- src/include/core.h
+++ src/include/core.h
@@ -215,6 +215,7 @@ struct ncclComm {
   struct cudaLaunchParams *myParams;
   int* intraCudaDevs;
   int* intraCGMode; // Whether we can use CUDA9 CGMD or not
+  int* intraCC; // Only to check all have the same ComputeCap and disable CGMode if not
   struct ncclColl args;
   void* argsptr;
 };
