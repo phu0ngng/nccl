b8490fe4200932abb3c3501b4879b6571b9132c0
Sylvain Jeaugey <sjeaugey@nvidia.com>
2018-01-10 15:21:18 -0800

    2.1.8-1

--- makefiles/version.mk
+++ makefiles/version.mk
@@ -1,7 +1,7 @@
 ##### version
 NCCL_MAJOR   := 2
 NCCL_MINOR   := 1
-NCCL_PATCH   := 7
+NCCL_PATCH   := 8
 NCCL_SUFFIX  :=
 PKG_REVISION := 1
 CUDA_VERSION ?= $(shell ls $(CUDA_LIB)/libcudart.so.* | head -1 | rev | cut -d "." -f -2 | rev)
