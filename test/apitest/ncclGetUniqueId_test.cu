4f68cd654043057c95152c837f325296958775e4
Ke Wen <kwen@nvidia.com>
2018-01-23 13:34:02 -0800

    apitest: add test for ncclGetUniqueIdFromEnv

--- test/apitest/ncclGetUniqueId_test.cu
+++ test/apitest/ncclGetUniqueId_test.cu
@@ -9,3 +9,22 @@ TEST(ncclGetUniqueId, basic) {
 TEST(ncclGetUniqueId, null) {
     EXPECT_EQ(ncclInvalidArgument, ncclGetUniqueId(NULL));
 }
+TEST(ncclGetUniqueId, env_id) {
+    int nDev;
+    ASSERT_EQ(cudaSuccess, cudaGetDeviceCount(&nDev));
+    setenv("NCCL_COMM_ID","127.0.0.1:16000",1);
+    ncclUniqueId id1, id2;
+    EXPECT_EQ(ncclSuccess, ncclGetUniqueId(&id1));
+    EXPECT_EQ(ncclSuccess, ncclGetUniqueIdFromEnv(&id2));
+    ncclComm_t comm[nDev];
+    EXPECT_EQ(ncclSuccess, ncclGroupStart());
+    for (int i = 0; i < nDev; i++) {
+      ASSERT_EQ(cudaSuccess, cudaSetDevice(i));
+      EXPECT_EQ(ncclSuccess, ncclCommInitRank(comm+i, nDev, (i==0) ? id1:id2, i));
+    }
+    EXPECT_EQ(ncclSuccess, ncclGroupEnd());
+    for (int i = 0; i < nDev; i++) {
+      EXPECT_EQ(ncclSuccess, ncclCommDestroy(comm[i]));
+    }
+    setenv("NCCL_COMM_ID","",1);
+}
