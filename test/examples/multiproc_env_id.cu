6702ed814077eb2d726324da959d53aa33d94c60
Ke Wen <kwen@nvidia.com>
2018-02-05 15:28:52 -0800

    cosmetic change

--- test/examples/multiproc_env_id.cu
+++ test/examples/multiproc_env_id.cu
@@ -84,15 +84,18 @@ int main(int argc, char* argv[])
   float *sendbuff, *recvbuff;
   cudaStream_t s;
 
-  // Get NCCL unique ID from environment variable NCCL_COMM_ID, saving the broadcast
+  // This is optional: delaying the root on purpose to see if the program fails
   if (myRank == 0) {
     char* env = getenv("ROOT_DELAY"); // delay in sec on purpose
     if (env && strlen(env) > 0) {
-      int delay = 0;
-      delay = atoi(env);
+      int delay = atoi(env);
       printf("[MPI Rank 0] Delaying root by %d seconds \n", delay);
       usleep(delay*1e6);
     }
+  }
+
+  // Get NCCL unique ID from environment variable NCCL_COMM_ID, saving the broadcast
+  if (myRank == 0) {
     ncclGetUniqueId(&id);
   } else {
     ncclGetUniqueIdFromEnv(&id);
