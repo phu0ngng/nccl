ce8c0cf88f2ca3316095aa956530db96a417c137
Ke Wen <kwen@nvidia.com>
2018-03-23 12:31:32 -0700

    test/api: Remove ncclGetUniqueIdFromEnv from api test

--- test/apitest/ncclGetUniqueId_test.cu
+++ test/apitest/ncclGetUniqueId_test.cu
@@ -9,25 +9,6 @@ TEST(ncclGetUniqueId, basic) {
 TEST(ncclGetUniqueId, null) {
     EXPECT_EQ(ncclInvalidArgument, ncclGetUniqueId(NULL));
 }
-TEST(ncclGetUniqueId, env_id_dual_api) {
-    int nDev;
-    ASSERT_EQ(cudaSuccess, cudaGetDeviceCount(&nDev));
-    setenv("NCCL_COMM_ID","127.0.0.1:16000",1);
-    ncclUniqueId id1, id2;
-    EXPECT_EQ(ncclSuccess, ncclGetUniqueId(&id1));
-    EXPECT_EQ(ncclSuccess, ncclGetUniqueIdFromEnv(&id2));
-    ncclComm_t comm[nDev];
-    EXPECT_EQ(ncclSuccess, ncclGroupStart());
-    for (int i = 0; i < nDev; i++) {
-      ASSERT_EQ(cudaSuccess, cudaSetDevice(i));
-      EXPECT_EQ(ncclSuccess, ncclCommInitRank(comm+i, nDev, (i==0) ? id1:id2, i));
-    }
-    EXPECT_EQ(ncclSuccess, ncclGroupEnd());
-    for (int i = 0; i < nDev; i++) {
-      EXPECT_EQ(ncclSuccess, ncclCommDestroy(comm[i]));
-    }
-    setenv("NCCL_COMM_ID","",1);
-}
 TEST(ncclGetUniqueId, env_id_single_api) {
     int nDev;
     ASSERT_EQ(cudaSuccess, cudaGetDeviceCount(&nDev));
