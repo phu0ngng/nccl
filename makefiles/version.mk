8273528e5bedc66d97a018e8a645f28695f1891b
Ke Wen <kwen@nvidia.com>
2018-03-25 14:42:54 -0700

    2.2.10NVSa4b2

--- makefiles/version.mk
+++ makefiles/version.mk
@@ -2,7 +2,7 @@
 NCCL_MAJOR   := 2
 NCCL_MINOR   := 2
 NCCL_PATCH   := 10
-NCCL_SUFFIX  := NVSa4b1
+NCCL_SUFFIX  := NVSa4b2
 PKG_REVISION := 1
 CUDA_VERSION ?= $(shell ls $(CUDA_LIB)/libcudart.so.* | head -1 | rev | cut -d "." -f -2 | rev)
 CUDA_MAJOR = $(shell echo $(CUDA_VERSION) | cut -d "." -f 1)
