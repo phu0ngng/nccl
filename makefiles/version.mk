a9d81f1a41757b96f9a466eb752f834c2813d9f6
Ke Wen <kwen@nvidia.com>
2018-03-27 14:24:17 -0700

    Clean unneeded cuda detection in version.mk

--- makefiles/version.mk
+++ makefiles/version.mk
@@ -4,8 +4,3 @@ NCCL_MINOR   := 2
 NCCL_PATCH   := 10
 NCCL_SUFFIX  := NVSa4b2
 PKG_REVISION := 1
-CUDA_VERSION ?= $(shell ls $(CUDA_LIB)/libcudart.so.* | head -1 | rev | cut -d "." -f -2 | rev)
-CUDA_MAJOR = $(shell echo $(CUDA_VERSION) | cut -d "." -f 1)
-CUDA_MINOR = $(shell echo $(CUDA_VERSION) | cut -d "." -f 2)
-
-CXXFLAGS  += -DCUDA_MAJOR=$(CUDA_MAJOR) -DCUDA_MINOR=$(CUDA_MINOR)
