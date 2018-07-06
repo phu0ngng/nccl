e9d0733f982ea7732a8e187be8fcc4d95c0c6546
David Addison <daddison@nvidia.com>
2018-07-05 17:47:57 -0700

    Replaced the new asserts by a WARN and ncclInternalError return
    Should be final change for BUG 2220324 fix

--- src/init.cu
+++ src/init.cu
@@ -504,10 +504,12 @@ static ncclResult_t initTransportsRank(struct ncclComm* comm, ncclUniqueId* comm
       intraRanks++;
     }
   }
-  TRACE("hostHash %lx intraRank %d intraRanks %d intraRank0 %d", rankInfos[rank].hostHash, intraRank, intraRanks, intraRank0);
-  assert(intraRank != -1);
-  assert(intraRank0 != -1);
-  assert(rankInfos[intraRank0].comm != NULL);
+  TRACE("hostHash[%d] %lx intraRank %d intraRanks %d intraRank0 %d", rank, rankInfos[rank].hostHash, intraRank, intraRanks, intraRank0);
+  if (intraRank == -1 || intraRank0 == -1 || rankInfos[intraRank0].comm == NULL) {
+    WARN("Failed to determine intra ranks hostHash[%d] %lx intraRank %d intraRanks %d intraRank0 %d",
+         rank, rankInfos[rank].hostHash, intraRank, intraRanks, intraRank0);
+    return ncclInternalError;
+  }
   NCCLCHECK(ncclCommSetIntra(comm, intraRank, intraRanks, rankInfos[intraRank0].comm));
 
   // Barrier
