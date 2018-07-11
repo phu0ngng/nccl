phu0ngng/nccl

commit 82a0831b6bc0c88c33b732732ca3f1175d6b3038

Fix a data corruption error when user sets LL threshold to 0

src/include/core.h
@@ -48,22 +48,20 @@ struct cudaLaunchParams
   nthreads = NCCL_LL_MIN_NTHREADS; \
   ll = 0; \
   size_t nr; \
-  while (nthreads < NCCL_LL_MAX_NTHREADS && ll == 0) { \
+  int factor = NCCL_LL_MAX_NTHREADS / NCCL_LL_MIN_NTHREADS; \
+  while (nthreads <= NCCL_LL_MAX_NTHREADS && ll == 0) { \
     nr = DIVUP(nbytes, (comm->ringThreshold*nthreads*comm->nRanks)); \
-    if (nr <= min(comm->nRings, NCCL_LL_MAX_NTHREADS / NCCL_LL_MIN_NTHREADS)) { /* avoid using few threads but many rings */ \
-      nrings = nr == 0 ? 1 : (int)nr; \
-      ll = 1; \
-    } else { \
+    if (nr <= factor || nthreads == NCCL_LL_MAX_NTHREADS) { /* avoid using few threads but many rings */ \
+      nrings = nr == 0 ? 1 : nr > comm->nRings ? comm->nRings : (int)nr; \
+      ll = nbytes > comm->nRanks*nrings*nthreads*comm->llThreshold ? 0 : 1; \
+    } \
+    if (ll == 0) { \
       nthreads = nthreads << 1; \
     } \
   } \
-  if (ll == 1) { \
-    break; \
+  if (ll == 0) { \
+    nthreads = comm->nThreads+1; \
   } \
-  nr = DIVUP(nbytes, (comm->ringThreshold*NCCL_LL_MAX_NTHREADS*comm->nRanks)); \
-  nrings = nr == 0 ? 1 : nr > comm->nRings ? comm->nRings : (int)nr; \
-  ll = nbytes > comm->nRanks*nrings*NCCL_LL_MAX_NTHREADS*comm->llThreshold ? 0 : 1; \
-  nthreads = ll ? NCCL_LL_MAX_NTHREADS : comm->nThreads+1; \
 } while (0)
 
 union ncclLLFifoLine {

commit 21432b07e68cfa2635ab2b983b56ee6ba90e785c

Fix a regression introduced by 82a0831b6bc0c88c33b732732ca3f1175d6b3038 Need to use ringThreshold instead of llThreshold for nthreads = 64 or 128

src/include/core.h
@@ -49,19 +49,23 @@ struct cudaLaunchParams
   ll = 0; \
   size_t nr; \
   int factor = NCCL_LL_MAX_NTHREADS / NCCL_LL_MIN_NTHREADS; \
-  while (nthreads <= NCCL_LL_MAX_NTHREADS && ll == 0) { \
+  ssize_t threshold = min(comm->llThreshold, comm->ringThreshold); \
+  while (nthreads < NCCL_LL_MAX_NTHREADS && ll == 0) { \
     nr = DIVUP(nbytes, (comm->ringThreshold*nthreads*comm->nRanks)); \
-    if (nr <= factor || nthreads == NCCL_LL_MAX_NTHREADS) { /* avoid using few threads but many rings */ \
+    if (nr <= factor) { /* avoid using few threads but many rings */ \
       nrings = nr == 0 ? 1 : nr > comm->nRings ? comm->nRings : (int)nr; \
-      ll = nbytes > comm->nRanks*nrings*nthreads*comm->llThreshold ? 0 : 1; \
+      ll = nbytes > comm->nRanks*nrings*nthreads*threshold ? 0 : 1; \
     } \
     if (ll == 0) { \
       nthreads = nthreads << 1; \
     } \
   } \
-  if (ll == 0) { \
-    nthreads = comm->nThreads+1; \
-  } \
+  if (ll == 1) break; \
+  nr = DIVUP(nbytes, (comm->ringThreshold*NCCL_LL_MAX_NTHREADS*comm->nRanks)); \
+  nr = nr == 0 ? 1 : nr > comm->nRings ? comm->nRings : nr; \
+  ll = nbytes > comm->nRanks*nr*NCCL_LL_MAX_NTHREADS*comm->llThreshold ? 0 : 1; \
+  nthreads = ll ? NCCL_LL_MAX_NTHREADS : comm->nThreads+1; \
+  nrings = ll ? (int)nr : comm->nRings; \
 } while (0)
 
 union ncclLLFifoLine {