56e51bb5b23dc4b2a0a4f72550071588b8dbccc5
Ke Wen <kwen@nvidia.com>
2018-07-26 12:18:24 -0700

    Don't know why K80+SoC requires this optimization to get BW (@32M) up from 4.1 GB/s to 4.5 GB/s

--- src/collectives/device/reduce_scatter.h
+++ src/collectives/device/reduce_scatter.h
@@ -34,6 +34,7 @@ __device__ void ncclReduceScatterKernel(struct CollectiveArgs* args) {
   const int nranks = comm->nRanks;
   const int buffSize = ring->buffSize / sizeof(T);
   const int sliceSize = buffSize / REDUCESCATTER_BUFCHUNKS;
+  const ssize_t loopSize = args->nRings*(ssize_t)sliceSize;
 
   if (tid == 0) {
     // Update in case we skipped some collectives
@@ -53,7 +54,7 @@ __device__ void ncclReduceScatterKernel(struct CollectiveArgs* args) {
   T * __restrict__ prevInput = (T*)ring->recv.conn.buff;
   T * __restrict__ nextOutput = (T*)ring->send.conn.buff;
 
-  for (ssize_t gridOffset = 0; gridOffset < size; gridOffset += args->nRings*sliceSize) {
+  for (ssize_t gridOffset = 0; gridOffset < size; gridOffset += loopSize) {
     int chunkSize = min(sliceSize, DIVUP(size-gridOffset,args->nRings));
     ALIGN_SIZE(chunkSize, nthreads*sizeof(uint64_t)/sizeof(T));
     ssize_t chunkOffset = gridOffset + bid*chunkSize;
