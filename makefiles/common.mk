2b41ab4e9cb88fc091c51c0c149523259b2d0778
Ke Wen <kwen@nvidia.com>
2018-09-06 10:10:56 -0700

    More updates to copyright

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2015-2016, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2015-2018, NVIDIA CORPORATION. All rights reserved.
 #
 # See LICENCE.txt for license information
 #
