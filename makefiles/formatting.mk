a4fe472f9c644aba2194dcf077fe22e038261c1c
Wojtek Wasko <wwasko@nvidia.com>
2018-09-10 11:56:26 -0700

    Formatting: no automatic dry run, build and use astyle 3.1
    Version of astyle has been fixed at 3.1 as it accepts all the
    necessary switches to make the formatting rules as NCCL-like as
    possible. The source code is no longer automatically checked for proper
    formatting; make doesn't check for whether astyle is present or not.
    
    Change-Id: Ife1676b1866d666ea7ccded1d6e3bb5b398a9967
    Reviewed-on: https://git-master.nvidia.com/r/1818244
    Reviewed-by: David Addison <daddison@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sylvain Jeaugey <sjeaugey@nvidia.com>
    Tested-by: Wojtek Wasko <wwasko@nvidia.com>

--- makefiles/formatting.mk
+++ makefiles/formatting.mk
@@ -9,23 +9,25 @@
 # default target.
 
 ASTYLE_FORMAT_OPTS=-Qv --style=java --indent-after-parens --indent-modifiers --indent-switches --indent-continuation=2 --keep-one-line-blocks --keep-one-line-statements --indent=spaces=2 --lineend=linux --suffix=none
+ASTYLEDIR := $(BUILDDIR)/contrib
+ASTYLETAR := $(ASTYLEDIR)/astyle.tar.gz
+ASTYLEBIN := $(ASTYLEDIR)/astyle/build/gcc/bin/astyle
+ASTYLEBLD := $(ASTYLEDIR)/astyle/build/gcc/
+ASTYLEVER := 3.1
+ASTYLEURL := "https://versaweb.dl.sourceforge.net/project/astyle/astyle/astyle%20$(ASTYLEVER)/astyle_$(ASTYLEVER)_linux.tar.gz"
 
-ifdef NOFORMATCHECK
-$(info Skipping code formatting checks on user request.)
-else
-ifeq "$(findstring $(MAKECMDGOALS),format)" ""
-ASTYLE_PRESENT := $(shell which astyle>/dev/null; echo $$?)
-ifeq ($(ASTYLE_PRESENT), 1)
-$(warning Astyle could not be found in the system. Please install astyle to enable code formatting checks.)
-else
-CODE_FORMATTED := $(shell astyle --dry-run $(ASTYLE_FORMAT_OPTS) $(FILESTOFORMAT) | grep -q "0 formatted"; echo $$?)
-ifneq ($(CODE_FORMATTED), 0)
-$(error Code is not formatted properly. Please run make format in the src directory.)
-endif # code formatted properly
-endif # astyle present in the system
-endif # "format" not a goal in this invocation
-endif # def NOFORMATCHECK
+$(ASTYLEDIR) :
+	@mkdir -p $(ASTYLEDIR)
+
+$(ASTYLETAR) : $(ASTYLEDIR)
+	@wget -q -O $(ASTYLETAR) $(ASTYLEURL)
+
+$(ASTYLEBLD) : $(ASTYLETAR)
+	@cd $(ASTYLEDIR) && tar xzf $(ASTYLETAR)
+
+$(ASTYLEBIN) : $(ASTYLEBLD)
+	${MAKE} -C $(ASTYLEBLD)
 
 .PHONY : format
-format :
-	@astyle $(ASTYLE_FORMAT_OPTS) $(FILESTOFORMAT)
+format : $(ASTYLEBIN)
+	@$(ASTYLEBIN) $(ASTYLE_FORMAT_OPTS) $(FILESTOFORMAT)
