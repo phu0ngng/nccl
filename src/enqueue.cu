c3f13fd6f37a9d6daaf748680d91a739410fe9d6
Ke Wen <kwen@nvidia.com>
2018-10-19 19:02:19 -0700

    Improve LL/non-LL switching

--- src/enqueue.cu
+++ src/enqueue.cu
@@ -267,30 +267,31 @@ static ncclResult_t getLoopInfo(struct ncclInfo* info) {
 
 static void getKernelInfo(struct ncclInfo* info, uint8_t* nChannels, uint16_t* nThreads, int* llMode) {
   // Start with minimum number of threads and LL.
-  int ll = 1, nt = NCCL_LL_MIN_NTHREADS, nc = 1;
+  int ll = 1, nt = NCCL_LL_MIN_NTHREADS, nc, maxChannels;
 
   // Compute thresholds and limits that users can override
   int perThreadLLThreshold = std::min(info->comm->threadThreshold, (ssize_t)NCCL_LL_CHANNEL_THRESHOLD);
   int maxLLNthreads = std::min(NCCL_LL_MAX_NTHREADS, info->comm->nThreads);
 
-  size_t sizePerThread;
-
   // Check if we have a fixed LL threshold.
-  if (info->comm->llThreshold >= 0 && info->nBytes >= info->comm->llThreshold) goto nonLL;
+  if (info->comm->llThreshold >= 0 && info->nBytes > info->comm->llThreshold) goto nonLL;
 
 restart:
+  // Compute max number of channels
+  maxChannels = (nt == maxLLNthreads) ? info->comm->nChannels : 1;
   // Compute the amount of work per thread per chunk
-  sizePerThread = info->nBytes / (nc*nt*info->nchunksPerLoop);
+  nc = std::max(1, (int)DIVUP(info->nBytes, nt*info->nchunksPerLoop*perThreadLLThreshold));
 
-  if (sizePerThread > perThreadLLThreshold) {
+  if (nc > maxChannels && nt*2 <= maxLLNthreads) {
     // We have too much work per LL thread. Try to do better.
-    if (nt*2 <= maxLLNthreads) { nt *= 2; goto restart; } // Try increasing nThreads
-    if (info->comm->nRanks > 4 && nc*2 <= info->comm->nChannels) { nc *= 2; goto restart; } // Then nChannels
+    nt *= 2; goto restart; // Try increasing nThreads
   }
 
   // nchannels and nthreads are already maxed out. Check if we need non-LL
-  // (unless LL has a fixed threshold and it is not an option).
-  if (info->comm->llThreshold < 0 && sizePerThread > info->comm->threadThreshold) ll = 0;
+  if (nc > maxChannels) {
+    nc = maxChannels;
+    if (info->nBytes > nc*nt*info->nchunksPerLoop*info->comm->threadThreshold) ll = 0;
+  }
 
   if (ll) {
     *llMode = 1;
