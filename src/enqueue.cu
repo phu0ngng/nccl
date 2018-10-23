b175177b5ac9b98be1c4c60290fe8d10f56c66c6
David Addison <daddison@nvidia.com>
2018-10-22 20:01:25 -0700

    Added root to INFO() enqueue logging

--- src/enqueue.cu
+++ src/enqueue.cu
@@ -402,11 +402,12 @@ static ncclResult_t saveKernel(struct ncclInfo* info) {
 
 
 ncclResult_t ncclEnqueueCheck(struct ncclInfo* info) {
-  INFO(COLL,"%s: opCount %lx sendbuff %p recvbuff %p count %zi datatype %d op %d comm %p [nranks=%d] stream %p",
-       info->opName, info->comm->opCount, info->sendbuff, info->recvbuff, info->count,
-       info->datatype, info->op, info->comm, info->comm->nRanks, info->stream);
-
   if (info->comm == NULL) return ncclInvalidArgument;
+
+  INFO(COLL,"%s: opCount %lx sendbuff %p recvbuff %p count %zi datatype %d op %d root %d comm %p [nranks=%d] stream %p",
+       info->opName, info->comm->opCount, info->sendbuff, info->recvbuff, info->count,
+       info->datatype, info->op, info->root, info->comm, info->comm->nRanks, info->stream);
+
   // Launch asynchronously if needed
   if (ncclAsyncMode()) {
     ncclResult_t ret = ncclSuccess;
