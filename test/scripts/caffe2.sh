60aa289800069b18c085daac6f889f06557d6ab6
Ke Wen <kwen@nvidia.com>
2019-01-03 10:41:56 -0800

    test: simplify test scripts and add timeout to dl runs

--- test/scripts/caffe2.sh
+++ test/scripts/caffe2.sh
@@ -14,7 +14,7 @@ if [ "$gpumodel" == "dgx1v" ]; then
   extra="--enable-tensor-core"
 fi
 
-NCCL_DISABLE_CHECKS=1 NCCL_DEBUG=WARN srun -p $gpumodel --exclusive \
+NCCL_DISABLE_CHECKS=1 NCCL_DEBUG=INFO srun -p $gpumodel -t 40 --exclusive \
 python $CAFFE2_ROOT/nvidia-examples/imagenet/train_resnet.py --train-lmdb /data/imagenet/train-lmdb-256x256 --skip-test --batch-size 1024 --num-iterations 1000 $extra --all-gpus --dtype float16 | \
 #tee /dev/stderr | awk -F'=' '/Epoch/ {if (lines++) sum += $NF} END {print sum/(lines-1)}' | \
 tee $result.out
--- test/scripts/mxnet.sh
+++ test/scripts/mxnet.sh
@@ -10,7 +10,7 @@ result="$path/mxnet"
 mxnet_path=/home/nightly/install/mxnet.github
 
 # Option "nccl" now stands for reduce/broadcast (same as "nccl_reducebcast" used before)
-NCCL_DISABLE_CHECKS=1 NCCL_DEBUG=WARN srun -p $gpumodel --exclusive \
+NCCL_DISABLE_CHECKS=1 NCCL_DEBUG=INFO srun -p $gpumodel -t 40 --exclusive \
 python $mxnet_path/example/image-classification/train_imagenet.py --gpu 0,1,2,3,4,5,6,7 --batch-size 1024 --num-epochs 1 --disp-batches 100 --network resnet-v1 --num-layers 50 --dtype float16 --benchmark 1 --kv-store nccl 2>&1 | \
 #awk '/Speed/ {if (lines++) sum += $5} END {print sum/(lines-1)}' | \
 tee $result.out
