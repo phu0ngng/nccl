db2ced4b77d23975cd2d0b681f9a406dc6e5c444
David Addison <daddison@nvidia.com>
2019-01-25 13:46:29 -0800

    Add NUMA support to PCI distance calculations
    Split PATH_SOC into PATH_NODE and PATH_SYS by querying the NUMA
    information of each PCI path. This now better matches the updated
    output of "nvidia-smi topo -m"

--- src/include/topo.h
+++ src/include/topo.h
@@ -11,6 +11,7 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdio.h>
 
 #define BUSID_SIZE (sizeof("0000:00:00.0"))
 #define BUSID_REDUCED_SIZE (sizeof("0000:00"))
@@ -30,14 +31,29 @@ static ncclResult_t getCudaPath(int cudaDev, char** path) {
   return ncclSuccess;
 }
 
+static int getNumaId(char *path) {
+  char npath[PATH_MAX];
+  snprintf(npath, PATH_MAX, "%s/numa_node", path);
+  npath[PATH_MAX-1] = '\0';
+
+  int numaId = -1;
+  FILE *file = fopen(npath, "r");
+  if (file == NULL) return -1;
+  if (fscanf(file, "%d", &numaId) == EOF) { fclose(file); return -1; }
+  fclose(file);
+
+  return numaId;
+}
+
 enum ncclPathDist {
-  PATH_PIX = 0,
-  PATH_PXB = 1,
-  PATH_PHB = 2,
-  PATH_SOC = 3
+  PATH_PIX  = 0,
+  PATH_PXB  = 1,
+  PATH_PHB  = 2,
+  PATH_NODE = 3,
+  PATH_SYS  = 4
 };
 
-static const char* pathDists[] = { "PIX", "PXB", "PHB", "SOC" };
+static const char* pathDists[] = { "PIX", "PXB", "PHB", "NODE", "SYS" };
 
 static int pciDistance(char* path1, char* path2) {
   int score = 0;
@@ -50,7 +66,13 @@ static int pciDistance(char* path1, char* path2) {
       if (same == 1) score++;
     }
   }
-  if (score <= 3) return PATH_SOC;
+  if (score <= 3) {
+    /* Split the former PATH_SOC distance into PATH_NODE and PATH_SYS based on numaId */
+    int numaId1 = getNumaId(path1);
+    int numaId2 = getNumaId(path2);
+    TRACE(NCCL_INIT, "depth %d score %d path1 %s numaId %d path2 %s numaId %d", depth, score, path1, numaId1, path2, numaId2);
+    return ((numaId1 == numaId2) ? PATH_NODE : PATH_SYS);
+  }
   if (score == 4) return PATH_PHB;
   if (score == depth-1) return PATH_PIX;
   return PATH_PXB;
