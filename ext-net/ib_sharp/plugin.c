7295b40113c1d718993df0ae80e6a6401704f83d
Ke Wen <kwen@nvidia.com>
2019-06-21 09:06:50 -0700

    Fix syntax error

--- ext-net/ib_sharp/plugin.c
+++ ext-net/ib_sharp/plugin.c
@@ -944,7 +944,8 @@ int ncclSharpOobBarrier(void *ctx) {
 int ncclSharpOobGather(void *ctx, int root, void *sbuf, void *rbuf, int size) {
   struct ncclSharpCollComm* cComm = (struct ncclSharpCollComm*)ctx;
   int nranks = cComm->nranks;
-  void *tmp = ncclIbMalloc(nranks*size);
+  void *tmp;
+  NCCLCHECK(ncclIbMalloc(&tmp, nranks*size));
   memcpy((void*)((ptrdiff_t)tmp + size*cComm->rank), sbuf, size);
   NCCLCHECK(ncclSharpAllGather(cComm, tmp, size));
   if (cComm->rank == root) {
@@ -956,7 +957,8 @@ int ncclSharpOobGather(void *ctx, int root, void *sbuf, void *rbuf, int size) {
 
 int ncclSharpOobBcast(void *ctx, void *buf, int size, int root) {
   struct ncclSharpCollComm* cComm = (struct ncclSharpCollComm*)ctx;
-  void *tmp = ncclIbMalloc(size*cComm->nranks);
+  void *tmp;
+  NCCLCHECK(ncclIbMalloc(&tmp, size*cComm->nranks));
   if (cComm->rank == root) {
     memcpy((void*)((ptrdiff_t)tmp+size*cComm->rank), buf, size);
   }
