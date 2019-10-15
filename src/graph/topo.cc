007606b26b08a1c7beff55d4920d89460ee66578
Sylvain Jeaugey <sjeaugey@nvidia.com>
2019-10-15 16:36:35 -0700

    Fix NUMA detection for NICs
    To merge NICs with multiple ports into one, we trimmed the last 2
    characters from the path (and modified the path permanently). This
    is a problem since we won't then able to figure out the NIC NUMA
    node with $path/numa_node. Systems where NICs were attached to a
    PCI switch which had GPUs were unaffected, since the PCI->CPU path
    was already created.
    
    To fix this, only create a copy where the subdevice is forced to
    zero and leave the path intact in the info struct.

--- src/graph/topo.cc
+++ src/graph/topo.cc
@@ -398,9 +398,12 @@ ncclResult_t ncclTopoComputeNetInfo(struct netInfo* netInfos, int ndev) {
     if (info->path && (ibGuid = getIbGuid(info->path)) != 0) {
       info->asic = ibGuid;
 
-      // Use "strlen(path)-2" to remove trailing subdevice and merge multi-port cards into one
-      info->path[strlen(info->path)-2]='\0';
-      NCCLCHECK(pciPathToInt64(info->path, strlen(info->path), 0, &info->nic));
+      // Ignore PCI subdevice when computing the ID to merge multi-port cards
+      // and make them use the same PCI link.
+      char* path = strdup(info->path);
+      path[strlen(path)-1]='0';
+      NCCLCHECK(pciPathToInt64(path, strlen(path), 0, &info->nic));
+      free(path);
 
       // Same PCI path -> different ports of the same NIC
       for (int i=0; i<n; i++) if (netInfos[i].nic == info->nic) info->port++;
