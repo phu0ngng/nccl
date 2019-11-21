bc74b4154f74dcfc6ae789cedc26cfd3724e2614
Ke Wen <kwen@nvidia.com>
2019-11-21 15:34:22 -0800

    Add CollTree depth

--- src/init.cc
+++ src/init.cc
@@ -726,6 +726,7 @@ static ncclResult_t initTransportsRank(struct ncclComm* comm, ncclUniqueId* comm
   NCCLCHECK(ncclTopoPostset(comm, nodesFirstRank, allTopoRanks, rings));
 
   // Set root of collTree to rank id nranks
+  int depth = comm->nRanks/comm->nNodes;
   int sendIndex = collNetGraph.pattern == NCCL_TOPO_PATTERN_TREE ? 0 : 1;  // send GPU index depends on topo pattern
   int sendEndIndex = (sendIndex+comm->localRanks-1)%comm->localRanks;
   for (int c=0; c<comm->nChannels/2; c++) {
@@ -736,6 +737,7 @@ static ncclResult_t initTransportsRank(struct ncclComm* comm, ncclUniqueId* comm
     if (rank == collNetGraph.intra[sendEndIndex+c*comm->localRanks]) { // is bottom of intra-node chain
       channel->collTreeUp.down[0] = channel->collTreeDn.down[0] = -1;
     }
+    channel->collTreeUp.depth = channel->collTreeDn.depth = depth;
     INFO(NCCL_INIT, "CollNet Channel %d rank %d up %d down %d", c, rank, channel->collTreeUp.up, channel->collTreeUp.down[0]);
   }
   int recvIndex = 0;  // recv GPU index is always 0
@@ -748,6 +750,7 @@ static ncclResult_t initTransportsRank(struct ncclComm* comm, ncclUniqueId* comm
     if (rank == collNetGraph.intra[recvEndIndex+c*comm->localRanks]) { // is bottom of intra-node chain
       channel->collTreeUp.down[0] = channel->collTreeDn.down[0] = -1;
     }
+    channel->collTreeUp.depth = channel->collTreeDn.depth = depth;
     INFO(NCCL_INIT, "CollNet Channel %d rank %d up %d down %d", c, rank, channel->collTreeDn.up, channel->collTreeDn.down[0]);
   }
 
