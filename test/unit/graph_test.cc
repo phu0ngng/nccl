8d0b2a02a1ad0c79bfe5baf48861b899896a23cc
Sylvain Jeaugey <sjeaugey@nvidia.com>
2020-01-16 17:12:00 -0800

    test/graph : Add DGX2+Delta unit test

--- test/unit/graph_test.cc
+++ test/unit/graph_test.cc
@@ -209,6 +209,7 @@ int main(int argc, const char* argv[]) {
     RUN("DGX-2V");
     RUN("XMAN-3");
     RUN("Luna");
+    RUN("DGX-2-Delta");
     RUN("GCP-NV");
     RUN("AWS-NV");
     RUN("Azure");
