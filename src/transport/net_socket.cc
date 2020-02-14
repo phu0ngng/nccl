d14388fefc6a611c531b0b98587ff9ad814897ab
Sylvain Jeaugey <sjeaugey@nvidia.com>
2020-02-14 10:54:12 -0800

    Fix speed detection for sockets

--- src/transport/net_socket.cc
+++ src/transport/net_socket.cc
@@ -75,7 +75,7 @@ ncclResult_t ncclSocketDevices(int* ndev) {
 static ncclResult_t ncclSocketGetSpeed(char* devName, int* speed) {
   *speed = 0;
   char speedPath[PATH_MAX];
-  sprintf(speedPath, "/sys/class/infiniband/%s/device/speed", devName);
+  sprintf(speedPath, "/sys/class/net/%s/speed", devName);
   int fd = open(speedPath, O_RDONLY);
   if (fd != -1) {
     char speedStr[] = "        ";
