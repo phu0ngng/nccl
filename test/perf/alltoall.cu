02e470b44ccde2401351a20aa8a52d0210d1abac
Anton Korzh <akorzh@akorzh-mlt.client.nvidia.com>
2020-02-27 08:29:05 -0800

    perf test bwfactor fix

--- test/perf/alltoall.cu
+++ test/perf/alltoall.cu
@@ -40,7 +40,7 @@ void AlltoAllGetBw(size_t count, int typesize, double sec, double* algBw, double
   double baseBw = (double)(count * typesize) / 1.0E9 / sec;
 
   *algBw = baseBw;
-  double factor = ((double)(nranks))/((double)(nranks-1));
+  double factor = ((double)(nranks-1))/((double)(nranks));
   *busBw = baseBw * factor;
 }
 
