be69a9365f644109c487eb9324dc2735da1f1058
Ke Wen <kwen@nvidia.com>
2020-04-08 22:58:16 -0700

    Model test: add env COMPARE_MODE to control which data to use as the "Default" value
    0 - from file (default), 1 - dry run

--- test/unit/model.cc
+++ test/unit/model.cc
@@ -24,7 +24,8 @@
 #undef NCCL_NUM_ALGORITHMS
 #define NCCL_NUM_ALGORITHMS 2
 
-int compactMode;
+int compactMode = 0;
+int compareMode = 0;
 float totalScore = 0.0;
 int totalNpoints = 0;
 
@@ -127,7 +128,7 @@ void runTopo(const char* xmlTopoFile, const char* platform, int ngpus, int nnode
     for (int a=0; a<NCCL_NUM_ALGORITHMS; a++) for (int p=0; p<NCCL_NUM_PROTOCOLS; p++) {
       printf(" %7s  / %7s  |", ncclAlgoStr[a], ncclProtoStr[p]);
     }
-    printf("%17s    |\n", "Default");
+    printf("%19s  |\n", compareMode == 0 ? "Default (file)" : "Default (dry run)");
     printf("          |");
     for (int i=0; i<m+1; i++) printf("[%9s] %9s|", "data", (i == m) ? "best" : "model");
     printf("\n");
@@ -137,10 +138,9 @@ void runTopo(const char* xmlTopoFile, const char* platform, int ngpus, int nnode
   }
 
   for (ssize_t size=8; size<(2LL<<32); size<<=1) {
-    float times[m+1];
-    float data[m+1];
+    float times[m+2];
+    float data[m+2];
     info.nBytes = size;
-    times[m] = -1.0; // Min time
     for (int a=0; a<NCCL_NUM_ALGORITHMS; a++) for (int p=0; p<NCCL_NUM_PROTOCOLS; p++) {
       int i = a*NCCL_NUM_PROTOCOLS+p;
       CHECK(ncclTopoGetAlgoTime(&info, a, p, times+i));
@@ -169,23 +169,27 @@ void runTopo(const char* xmlTopoFile, const char* platform, int ngpus, int nnode
     float bestModelTime = -1.0;
     int bestModel = -1;
     for (int i=0; i<m; i++) {
-      if (times[i] < 0 || data[i] < 0) continue;
+      if (data[i] < 0) continue;
+      // find best data
+      if (times[m] < 0 || times[m] > data[i]) times[m] = data[i];
       // find best model
+      if (times[i] < 0) continue;
       if (bestModelTime < 0 || bestModelTime > times[i]) {
         bestModelTime = times[i];
         bestModel = i;
       }
-      // find best data
-      if (times[m] < 0 || times[m] > data[i]) times[m] = data[i];
     }
-    // Set data[m] to the one picked by the best model (as if we have done a default run)
-    if (bestModel != -1) data[m] = data[bestModel];
-    else data[m] = -1;
+    // Add a column
+    times[m+1] = times[m];
+    // Store the data picked by the best model (as if we have done a dry default run)
+    data[m+1] = -1;
+    if (bestModel != -1) data[m+1] = data[bestModel];
 
     if (!compactMode) {
       printf("%10ld|", size);
       for (int i=0; i<m+1; i++) {
         float delta;
+        if (i == m && compareMode != 0) i = m+1;
         if (data[i] != -1.0) {
           printf("[%9.1f] ", data[i]);
           delta = 1-(times[i]/data[i]);
@@ -197,15 +201,16 @@ void runTopo(const char* xmlTopoFile, const char* platform, int ngpus, int nnode
           else printf("%c[0;32m", 0x1b);
         } else {
           printf("%11s ", "");
-          if (i != m && times[i] == times[m]) printf("%c[0;32m", 0x1b);
+          if (i < m && times[i] == times[m]) printf("%c[0;32m", 0x1b);
         }
         printf("%9.1f", times[i]);
-        if ((data[i] != -1.0) || (i != m && times[i] == times[m])) printf("%c[00m", 0x1b);
+        if ((data[i] != -1.0) || (i < m && times[i] == times[m])) printf("%c[00m", 0x1b);
         printf("|");
       }
       printf("\n");
     } else {
-      float s = times[m]/data[m];
+      int n = (compareMode == 0) ? m : m+1; // which data to compare: m = data from file, m+1 = data chosen by model
+      float s = times[n]/data[n];
       if (s < 0.8) printf("%c[0;31m#", 0x1b);
       else if (s < .95) printf("%c[0;33mX", 0x1b);
       else if (s > 1.1) printf("%c[0;34mO", 0x1b);
@@ -241,8 +246,8 @@ void runPlatform(const char* platform, int ngpus, int nnodes, ncclFunc_t coll) {
 #define RUN(...) runPlatform(__VA_ARGS__)
 
 int main(int argc, const char* argv[]) {
-  char* str = getenv("COMPACT_MODE");
-  compactMode = str ? atoi(str) : 0;
+  char* str = getenv("COMPARE_MODE");
+  compareMode = str ? atoi(str) : 0;
 
   setlinebuf(stdout);
   if (argc > 4) {
@@ -257,6 +262,7 @@ int main(int argc, const char* argv[]) {
     RUN(argv[1], atoi(argv[2]), atoi(argv[3]), (ncclFunc_t)coll);
   } else if (argc > 1) {
     printf("Usage : %s <platform> <ngpus> <nnodes> <collective>\n", argv[0]);
+    printf("Set COMPARE_MODE to select which data to use as the Default: 0 - from file, 1 - dry run\n");
     return 1;
   } else {
     compactMode = 1;
