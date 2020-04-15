32ed984995660c6a8568514fb8fb5633d2592c5b
Sylvain Jeaugey <sjeaugey@nvidia.com>
2020-04-15 10:53:55 -0700

    Fix >2G local copy through a single channel.
    ReduceOrCopy takes an int as number of elements. Split the copy
    into 1G elements.

--- src/collectives/device/sendrecv.h
+++ src/collectives/device/sendrecv.h
@@ -21,8 +21,15 @@ __device__ void ncclSendRecvKernel(struct CollectiveArgs* args) {
 
   if (args->p2p.delta == 0) {
     if (tid < nthreads && sendbuff != recvbuff) {
-      // local copy
-      ReduceOrCopyMulti<UNROLL, FUNC, T, 1, 1, 1, 1>(tid, nthreads, 1, &sendbuff, 1, &recvbuff, args->p2p.sendCount);
+      // local copy : ReduceOrCopyMulti takes an int as number of elements,
+      // so we split it in blocks of 1G elements.
+      int blockSize = 1<<30;
+      for (size_t offset=0; offset<args->p2p.sendCount; offset += blockSize) {
+        size_t remaining = args->p2p.sendCount - offset;
+        if (remaining < blockSize) blockSize = remaining;
+        ReduceOrCopyMulti<UNROLL, FUNC, T, 1, 1, 1, 1>(tid, nthreads, 1, &sendbuff, 1, &recvbuff, blockSize);
+        sendbuff += blockSize; recvbuff += blockSize;
+      }
     }
     return;
   }
