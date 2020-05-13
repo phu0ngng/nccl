c1c8ec6345f4ba962b91bf7d10721e36f062e528
Ke Wen <kwen@nvidia.com>
2020-05-13 03:38:00 -0700

    Restore tuning after Luna channel speed increases from 21 to 24 GB/s
    The speed increase is in 52c517

--- src/graph/tuning.cc
+++ src/graph/tuning.cc
@@ -107,7 +107,7 @@ ncclResult_t ncclTopoTuneModel(struct ncclComm* comm, int minCompCap, int maxCom
       for (int p=0; p<NCCL_NUM_PROTOCOLS; p++) {
         float speed = comm->nNodes <= 2 || a == NCCL_ALGO_COLLNET ? graphs[a]->speedIntra : graphs[a]->speedInter;
         float busBw = graphs[a]->nChannels * speed;
-        if (compCap80) busBw *= 0.92;
+        if (compCap80) busBw = std::min(busBw*(comm->nNodes>1 ? 0.83:1.0), 235.0);
 
         // Various model refinements
         if (a == NCCL_ALGO_RING && p == NCCL_PROTO_LL)    busBw *= (comm->nNodes > 1 || coll == ncclCollAllReduce || coll == ncclCollReduce) ? 1.0/4.0 : 1.0/3.0;
