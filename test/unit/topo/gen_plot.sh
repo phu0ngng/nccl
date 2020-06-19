48221ecaf2093263f64cad00cd659ba783b4ad61
David Addison <daddison@nvidia.com>
2020-06-19 14:05:59 -0700

    A few minor tweaks to this plot file

--- test/unit/topo/gen_plot.sh
+++ test/unit/topo/gen_plot.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 platform=$1
 nodes=$2
-mode=$3
-coll=$4
+mode=${3:-"time"}
+coll=${4:-"AllReduce"}
 gpus=$5
 
 if [ "$nodes" == "" ]; then
@@ -16,8 +18,8 @@ if [ "$nodes" == "" ]; then
   exit 1
 fi
 
-if [ "$coll" == "" ]; then coll=AllReduce; fi
-if [ "$mode" == "" ]; then mode=time; fi
+#if [ "$coll" == "" ]; then coll=AllReduce; fi
+#if [ "$mode" == "" ]; then mode=time; fi
 if [ "$gpus" == "" ]; then 
   gpus=1
   for g in `ls $platform/data/`; do
@@ -48,23 +50,26 @@ if [ "$mode" != "time" ]; then
   done
 fi
 
+OUTPUT="nccl-${coll}-${mode}-${platform}-${gpus}x${nodes}.png"
 (
 echo    'set term pngcairo size 1200, 800'
-echo    'set output "nccl-'$mode'-'$platform'-'$gpus'x'$nodes'.png"'
+echo    'set output "'$OUTPUT'"'
 if [ "$mode" == "time" ]; then echo    'set logscale y'; fi
-echo    'set title "NCCL '$coll' '$mode', '$gpus' GPUs('$platform') x '$nodes'"'
+echo    'set title "NCCL '$coll' '$mode', '$platform', GPUs('$gpus') x '$nodes'"'
 echo -n 'set xtics (                 "8"   0, "16"   1, "32"   2, "64"   3, "128"   4, "256"   5, "512"   6, '
 echo -n ' "1K"  7, "2K"  8, "4K"  9, "8K" 10, "16K" 11, "32K" 12, "64K" 13, "128K" 14, "256K" 15, "512K" 16, '
 echo -n ' "1M" 17, "2M" 18, "4M" 19, "8M" 20, "16M" 21, "32M" 22, "64M" 23, "128M" 24, "256M" 25, "512M" 26, '
 echo    ' "1G" 27, "2G" 28, "4G" 29)'
 echo    'set xtics rotate by 60 right'
-echo -n "plot '$path/$mode.txt'             title 'Default'     with lines lt rgb "'"#333333"'" lw 2 dt 3,"
-echo -n "     '$path/Ring/LL/$mode.txt'     title 'Ring/LL'     with lines lt rgb "'"#9999FF"'" lw 1,"
-echo -n "     '$path/Ring/LL128/$mode.txt'  title 'Ring/LL128'  with lines lt rgb "'"#6666FF"'" lw 1,"
-echo -n "     '$path/Ring/Simple/$mode.txt' title 'Ring/Simple' with lines lt rgb "'"#0000FF"'" lw 1,"
-echo -n "     '$path/Tree/LL/$mode.txt'     title 'Tree/LL'     with lines lt rgb "'"#FF9999"'" lw 1,"
-echo -n "     '$path/Tree/LL128/$mode.txt'  title 'Tree/LL128'  with lines lt rgb "'"#FF6666"'" lw 1,"
-echo    "     '$path/Tree/Simple/$mode.txt' title 'Tree/Simple' with lines lt rgb "'"#FF0000"'" lw 1"
+echo    'set grid xtics ytics mytics'
+echo    'set key left'
+echo -n "plot '$path/$mode.txt'             title 'Default'     with linespoint lt rgb "'"#333333"'" lw 2 dt 3,"
+echo -n "     '$path/Ring/LL/$mode.txt'     title 'Ring/LL'     with linespoint lt rgb "'"#9999FF"'" lw 1,"
+echo -n "     '$path/Ring/LL128/$mode.txt'  title 'Ring/LL128'  with linespoint lt rgb "'"#6666FF"'" lw 1,"
+echo -n "     '$path/Ring/Simple/$mode.txt' title 'Ring/Simple' with linespoint lt rgb "'"#0000FF"'" lw 1,"
+echo -n "     '$path/Tree/LL/$mode.txt'     title 'Tree/LL'     with linespoint lt rgb "'"#FF9999"'" lw 1,"
+echo -n "     '$path/Tree/LL128/$mode.txt'  title 'Tree/LL128'  with linespoint lt rgb "'"#FF6666"'" lw 1,"
+echo    "     '$path/Tree/Simple/$mode.txt' title 'Tree/Simple' with linespoint lt rgb "'"#FF0000"'" lw 1"
 ) | gnuplot
 
 if [ "$mode" != "time" ]; then
@@ -73,4 +78,6 @@ if [ "$mode" != "time" ]; then
     rm $p/$mode.txt
   done
 fi
+
+echo $OUTPUT
 
