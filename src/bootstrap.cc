912a09d8c1157dd61bc0af5be28c406d59d5711d
John Bachan <jbachan@nvidia.com>
2020-10-05 14:32:00 -0700

    Fixed issue where allocThread's were lingering well beyond their communicators lifetime. The poll timeout was set crazy high (a misinterpration of units) so now this thread will react within ~100ms. A better fix would be to have the stop condition be poll()'d for by the thread to eliminate any lag.

--- src/bootstrap.cc
+++ src/bootstrap.cc
@@ -260,7 +260,7 @@ void* ncclRemoteMemAllocationService(void* args) {
 
   int nbuffers = 0;
   while (state->stop == 0 || (state->stop == 1 && nbuffers > 0)) {
-    if (int error = poll(pollfds, MAX_SEGMENTS+1, 1000000) < 0) {
+    if (int error = poll(pollfds, MAX_SEGMENTS+1, 100/*ms*/) < 0) {
       WARN("[Rem Allocator] Poll failed with error %d", error);
       return NULL;
     }
@@ -496,7 +496,12 @@ ncclResult_t bootstrapClose(void* commState) {
   close(state->extListenFd);
   close(state->extRingSendFd);
   close(state->extRingRecvFd);
+  
   state->allocState->stop = 1;
+  
+  // Join the allocThread so we catch resrouce leaks as being hung here
+  // pthread_join(state->allocThread, nullptr);
+  
   free(state->peerCommAddresses);
   free(state->peerAllocAddresses);
   free(state);
