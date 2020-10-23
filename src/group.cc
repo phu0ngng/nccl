f297890dd1c07004284557384c15cdf1fc3d3cbc
Sylvain Jeaugey <sjeaugey@nvidia.com>
2020-10-22 17:37:01 -0700

    Fix hang in scatter/gather intra-node.
    Tuning the number of channels based on the number of operation was
    causing inconsistent number of channels in scatter and gather.

--- src/group.cc
+++ src/group.cc
@@ -224,11 +224,10 @@ ncclResult_t ncclGroupEnd() {
       // Try to use all channels
       int nChannelsMax = comm->p2pnChannelsPerPeer;
       int nChannelsMin = nChannelsMax;
-      int p2pMaxCount = std::max(comm->p2pSendCount, comm->p2pRecvCount);
       // Try to use all channels, but one channel per operation.
-      while (nChannelsMin*p2pMaxCount > comm->p2pnChannels && nChannelsMin > 1) nChannelsMin /= 2;
+      while (nChannelsMin*comm->nRanks > comm->p2pnChannels && nChannelsMin > 1) nChannelsMin /= 2;
       // Avoid overloading channels with 8+ operations as we loose the sync warp, hence a bit of bandwidth.
-      while (nChannelsMax*p2pMaxCount > comm->p2pnChannels*4 && nChannelsMax > 1) nChannelsMax /= 2;
+      while (nChannelsMax*comm->nRanks > comm->p2pnChannels*4 && nChannelsMax > 1) nChannelsMax /= 2;
 
       while (comm->p2pSendCount > 0 || comm->p2pRecvCount > 0) {
         // schedule delta 0, +1, -1, +2, -2, ...
