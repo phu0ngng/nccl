ef0a63d081b9e86fc84df918280bf36426b4ca77
Sylvain Jeaugey <sjeaugey@nvidia.com>
2020-11-02 16:53:43 -0800

    Fix Sendrecv tests on platforms with only one GPU.
    Probably the issue on bug 200671942.

--- test/apitest/ncclSendRecv_test.cu
+++ test/apitest/ncclSendRecv_test.cu
@@ -10,16 +10,18 @@ TYPED_TEST_CASE(ncclSendRecv_test, testDataTypes);
 TYPED_TEST(ncclSendRecv_test, DISABLED_simple) {
     size_t size = std::min(this->N, 1024 * 1024);
     ASSERT_EQ(ncclSuccess, ncclGroupStart());
-    ASSERT_EQ(ncclSuccess,
-              ncclSend(this->sendbuffs[0], size,
-                            this->DataType(), 1,
-                            this->comms[0], this->streams[0]))
+    if (this->nVis >= 2) {
+      ASSERT_EQ(ncclSuccess,
+          ncclSend(this->sendbuffs[0], size,
+            this->DataType(), 1,
+            this->comms[0], this->streams[0]))
         << "i" << 0 << ", " << std::endl;
-    ASSERT_EQ(ncclSuccess,
-              ncclRecv(this->recvbuffs[1], size,
-                            this->DataType(), 0,
-                            this->comms[1], this->streams[1]))
+      ASSERT_EQ(ncclSuccess,
+          ncclRecv(this->recvbuffs[1], size,
+            this->DataType(), 0,
+            this->comms[1], this->streams[1]))
         << "i" << 1 << ", " << std::endl;
+    }
     ASSERT_EQ(ncclSuccess, ncclGroupEnd());
 };
 TYPED_TEST(ncclSendRecv_test, ring) {
