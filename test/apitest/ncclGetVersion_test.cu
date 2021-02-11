5a7d0d6e6fc5c57bd7eb2a8a99c3b6a5e9719b34
David Addison <daddison@nvidia.com>
2021-02-11 12:03:26 -0800

    Modified NCCL_VERSION to support MINOR versions up to 99
    The previous NCCL_VERSION macro only allowed for minor numbers
    up to 9 before it would overflow in the NCCL_VERSION_CODE number

--- test/apitest/ncclGetVersion_test.cu
+++ test/apitest/ncclGetVersion_test.cu
@@ -3,6 +3,27 @@ TEST(ncclGetVersion, basic) {
     EXPECT_EQ(ncclSuccess, ncclGetVersion(&version));
     EXPECT_EQ(version, NCCL_VERSION_CODE);
 }
+TEST(ncclGetVersion, new_gt_old) {
+    int version;
+    EXPECT_EQ(ncclSuccess, ncclGetVersion(&version));
+    EXPECT_GT(version, NCCL_VERSION(2,8,4));
+}
+TEST(ncclGetVersion, code_gt_old) {
+    EXPECT_GT(NCCL_VERSION_CODE, NCCL_VERSION(2,8,4));
+}
+TEST(ncclGetVersion, code) {
+    EXPECT_EQ(NCCL_VERSION_CODE, NCCL_VERSION(NCCL_MAJOR,NCCL_MINOR,NCCL_PATCH));
+}
+TEST(ncclGetVersion, macro) {
+    int version;
+    EXPECT_EQ(ncclSuccess, ncclGetVersion(&version));
+    EXPECT_EQ(version, NCCL_VERSION(NCCL_MAJOR,NCCL_MINOR,NCCL_PATCH));
+}
+TEST(ncclGetVersion, macro_new_gt_old) {
+    int version;
+    EXPECT_EQ(ncclSuccess, ncclGetVersion(&version));
+    EXPECT_GT(NCCL_VERSION(2,9,0), NCCL_VERSION(2,8,4));
+}
 TEST(ncclGetVersion, null) {
     EXPECT_EQ(ncclInvalidArgument, ncclGetVersion(NULL));
 }
