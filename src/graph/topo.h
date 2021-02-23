a6a4eb0e9c5fc405c9041a9f18f1a525ad118fd6
Sylvain Jeaugey <sjeaugey@nvidia.com>
2021-02-23 13:52:40 -0800

    Remove unused INTEL_P2P macro

--- src/graph/topo.h
+++ src/graph/topo.h
@@ -27,7 +27,6 @@
 
 // Intel CPU convert GPU P2P traffic into 64B PCI TLPs, so GPU
 // to GPU traffic consumes more PCI bandwidth.
-#define INTEL_P2P(speed) (speed*5/6
 #define INTEL_P2P_OVERHEAD(speed) (speed*6/5)
 
 #define NCCL_TOPO_NODE_TYPES 7
