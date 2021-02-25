b85cfb631b4f05ff8c35f56d2651f1bc68c2cce0
Ke Wen <kwen@nvidia.com>
2021-02-25 10:11:10 -0800

    Direct: adjust chunksize tuning for merged-channel oneshot

--- src/enqueue.cc
+++ src/enqueue.cc
@@ -403,8 +403,9 @@ static ncclResult_t computeColl(struct ncclInfo* info /* input */, struct ncclWo
     while (info->nBytes / (info->nChannels*chunkSize) < info->comm->channels[0].collTree.depth && (info->nChannels*chunkSize) > 32768) chunkSize /= 2;
     while (info->nBytes / (info->nChannels*chunkSize) < 1 && chunkSize >128) chunkSize /= 2;
 #else
-    while (info->nBytes / (info->nChannels*chunkSize) < info->comm->channels[0].collTree.depth*32 && chunkSize > 131072) chunkSize /= 2;
-    while (info->nBytes / (info->nChannels*chunkSize) < info->comm->channels[0].collTree.depth*4 && chunkSize > 32768) chunkSize /= 2;
+    while (info->nBytes / (info->nChannels*chunkSize) < info->comm->channels[0].collTree.depth*256 && chunkSize > 131072) chunkSize /= 2;
+    while (info->nBytes / (info->nChannels*chunkSize) < info->comm->channels[0].collTree.depth*32 && chunkSize > 32768) chunkSize /= 2;
+    while (info->nBytes / (info->nChannels*chunkSize) < info->comm->channels[0].collTree.depth*4 && chunkSize > 16384) chunkSize /= 2;
 #endif
     // Use lastChunkSize as chunkSize
     work->coll.lastChunkSize = chunkSize / ncclTypeSize(info->datatype);
