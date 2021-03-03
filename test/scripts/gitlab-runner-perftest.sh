a960aeb7e726e9244a0c0552c0e9734adb94d285
Sylvain Jeaugey <sjeaugey@nvidia.com>
2021-03-02 17:50:44 -0800

    gitlab-ci: add more perf tests, reduce size on K80, increase size on DGX

--- test/scripts/gitlab-runner-perftest.sh
+++ test/scripts/gitlab-runner-perftest.sh
@@ -3,5 +3,17 @@
 export OPAL_PREFIX=$MPI_HOME
 export LD_LIBRARY_PATH=$MPI_HOME/lib:$PWD/build/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
-$SALLOC $MPI_HOME/bin/mpirun ./build/test/perf/all_reduce_perf -b 8 -e 1G -f 2
-$SALLOC $MPI_HOME/bin/mpirun ./build/test/perf/alltoall_perf -b 8 -e 1G -f 2
+max=$1
+if [ "$max" == "" ]; then max=1G; fi
+
+opts="-n 5 -w 1"
+
+range="-b 8 -e $max -f 2"
+for func in all_reduce reduce reduce_scatter broadcast all_gather alltoall gather scatter sendrecv hypercube; do
+  $SALLOC $MPI_HOME/bin/mpirun ./build/test/perf/${func}_perf $range $opts
+done
+
+rangetype="-b 16M -e 16M"
+for func in all_reduce reduce reduce_scatter; do
+  $SALLOC $MPI_HOME/bin/mpirun ./build/test/perf/all_reduce_perf $rangetype $opts
+done
