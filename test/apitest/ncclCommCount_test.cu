8b8cbc40880488bc5276f712fca251ff90518a26
Sylvain Jeaugey <sjeaugey@nvidia.com>
2021-03-04 12:57:15 -0800

    API Tests time optimization
    Avoid creating/destroying communicators and buffers for each test,
    only for those specific to comm creation. Make communicators
    persistent.

--- test/apitest/ncclCommCount_test.cu
+++ test/apitest/ncclCommCount_test.cu
@@ -1,26 +1,17 @@
+#include "ncclCommon_test.cuh"
 class ncclCommCount_test : public ::testing::Test {
   protected:
     ncclComm_t* comms = NULL;
-    int* devList = NULL;
     int nVis = 0, ndev = 0;
     int count = -1;
     virtual void SetUp() {
-        ASSERT_EQ(cudaSuccess, cudaGetDeviceCount(&nVis));
+        comms = ncclCommon_getComms(&nVis);
         ndev = nVis;
-        comms = (ncclComm_t*)calloc(ndev, sizeof(ncclComm_t));
     };
     virtual void TearDown() {
-        free(devList);
-        if (comms != NULL) {
-            for (int i = 0; i < nVis; ++i) {
-                ncclCommDestroy(comms[i]);
-            }
-            free(comms);
-        }
     };
 };
 TEST_F(ncclCommCount_test, basic) {
-    ASSERT_EQ(ncclSuccess, ncclCommInitAll(comms, ndev, NULL));
     for (int i = 0; i < ndev; ++i) {
         ASSERT_EQ(ncclSuccess, ncclCommCount(comms[i], &count));
         ASSERT_EQ(ndev, count);
@@ -32,7 +23,6 @@ TEST_F(ncclCommCount_test, comm_null) {
 };
 // 2.
 TEST_F(ncclCommCount_test, count_null) {
-    ASSERT_EQ(ncclSuccess, ncclCommInitAll(comms, ndev, NULL));
     for (int i = 0; i < ndev; ++i) {
         ASSERT_EQ(ncclInvalidArgument, ncclCommCount(comms[i], NULL));
     }
@@ -40,9 +30,13 @@ TEST_F(ncclCommCount_test, count_null) {
 // 3.
 TEST_F(ncclCommCount_test, comm_some) {
     ndev = nVis - (nVis > 1 ? 1 : 0);
+    ncclComm_t comms[ndev];
     ASSERT_EQ(ncclSuccess, ncclCommInitAll(comms, ndev, NULL));
     for (int i = 0; i < ndev; ++i) {
         ASSERT_EQ(ncclSuccess, ncclCommCount(comms[i], &count));
         ASSERT_EQ(ndev, count);
-    };
+    }
+    for (int i = 0; i < ndev; ++i) {
+        ASSERT_EQ(ncclSuccess, ncclCommDestroy(comms[i]));
+    }
 }
