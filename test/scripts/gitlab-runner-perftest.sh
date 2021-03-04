e6f544299bff26dd357a2b06a6646bb74db8ff89
Sylvain Jeaugey <sjeaugey@nvidia.com>
2021-03-04 13:26:15 -0800

    gitlab-ci: Add header before each test

--- test/scripts/gitlab-runner-perftest.sh
+++ test/scripts/gitlab-runner-perftest.sh
@@ -10,10 +10,12 @@ opts="-n 5 -w 1"
 
 range="-b 8 -e $max -f 2"
 for func in all_reduce reduce reduce_scatter broadcast all_gather alltoall gather scatter sendrecv hypercube; do
+  echo "=============================== $func (all sizes) ================================="
   $SALLOC $MPI_HOME/bin/mpirun ./build/test/perf/${func}_perf $range $opts
 done
 
 rangetype="-b 16M -e 16M -o all -d all"
 for func in all_reduce reduce reduce_scatter; do
+  echo "=============================== $func (all ops/dtype)  ================================="
   $SALLOC $MPI_HOME/bin/mpirun ./build/test/perf/all_reduce_perf $rangetype $opts
 done
