12cfb447d596ab82a84be7b297c91487491b9bfa
Ke Wen <kwen@nvidia.com>
2021-03-07 17:08:29 -0800

    Direct: update chunksize tuning after data corruption fix

--- src/enqueue.cc
+++ src/enqueue.cc
@@ -403,8 +403,9 @@ static ncclResult_t computeColl(struct ncclInfo* info /* input */, struct ncclWo
     while (info->nBytes / (info->nChannels*chunkSize) < info->comm->channels[0].collTree.depth && (info->nChannels*chunkSize) > 32768) chunkSize /= 2;
     while (info->nBytes / (info->nChannels*chunkSize) < 1 && chunkSize >128) chunkSize /= 2;
 #else
-    while (info->nBytes / (info->nChannels*info->comm->channels[0].collTree.nHeads*chunkSize) < info->comm->channels[0].collTree.depth*32 && chunkSize > 131072) chunkSize /= 2;
-    while (info->nBytes / (info->nChannels*info->comm->channels[0].collTree.nHeads*chunkSize) < info->comm->channels[0].collTree.depth*4 && chunkSize > 32768) chunkSize /= 2;
+    while (info->nBytes / (info->nChannels*info->comm->channels[0].collTree.nHeads*chunkSize) < info->comm->channels[0].collTree.depth*32 && chunkSize > 262144) chunkSize /= 2;
+    while (info->nBytes / (info->nChannels*info->comm->channels[0].collTree.nHeads*chunkSize) < info->comm->channels[0].collTree.depth*16 && chunkSize > 131072) chunkSize /= 2;
+    while (info->nBytes / (info->nChannels*info->comm->channels[0].collTree.nHeads*chunkSize) < info->comm->channels[0].collTree.depth*8 && chunkSize > 32768) chunkSize /= 2;
     while (info->nBytes / (info->nChannels*info->comm->channels[0].collTree.nHeads*chunkSize) < info->comm->channels[0].collTree.depth/2 && chunkSize > 16384) chunkSize /= 2;
 #endif
     // Use lastChunkSize as chunkSize
