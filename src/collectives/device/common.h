7a26ad367c2059223c6dc90613bf83811e7504a2
Sylvain Jeaugey <sjeaugey@nvidia.com>
2021-04-12 15:41:19 -0700

    Copyright update

--- src/collectives/device/common.h
+++ src/collectives/device/common.h
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (c) 2017-2020, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2017-2021, NVIDIA CORPORATION. All rights reserved.
  *
  * See LICENSE.txt for license information
  ************************************************************************/
