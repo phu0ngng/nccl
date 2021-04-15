b2d9222e191ac10c5caace748632d72d2cc4862c
John Bachan <jbachan@nvidia.com>
2021-04-15 16:07:57 -0700

    Added performance regression scripts to test/scripts that will sweep all combos of perf executable, protocol, algo, datatype, redop for 2-gpus. The intent is to catch device side regressions due to poorly formed SASS.
    Added a manual-only perf regression job to gitlab CI/CD just for
    gpu-comms-head for now.

---
+++ test/scripts/gitlab-runner-perf-regression.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+if [[ ! -d ./test/perf ]]; then
+  >&2 echo "Script must be run from top-level nccl directory."
+  exit 1
+fi
+if [[ ! -d ./build/lib ]]; then
+  >&2 echo "'./build' must contain built nccl lib and tests."
+  exit 1
+fi
+
+export OPAL_PREFIX=$MPI_HOME
+export LD_LIBRARY_PATH=$MPI_HOME/lib:$PWD/build/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
+function absify() {
+  python3 -c 'import os; import sys; sys.stdout.write(os.path.abspath(sys.argv[1]))' "$1"
+}
+
+build_baseline="build-${REGRESSION_BASELINE}"
+
+# Build the baseline version if necessary. We manually check since the makefile
+# will use file mtime's which will have been just updated by the preceding
+# `git checkout` so will always conclude to rebuild.
+if [[ ! -e "$build_baseline"/test/perf/all_reduce_perf ]]; then
+  git checkout "$REGRESSION_BASELINE" &&
+  make -j src.build test.build BUILDDIR="$build_baseline" MPI=1 MPI_HOME="$MPI_HOME" CUDA_HOME="$CUDA_HOME" &&
+  git checkout "$CI_COMMIT_REF_NAME"
+  ok=$?
+else
+  ok=0
+fi
+
+if [[ $ok == 0 ]]; then
+  # run regression suite
+  ./test/scripts/perf_regression.py old="$build_baseline" new=build --check 0
+fi
---
+++ test/scripts/perf_regression.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+import os
+import sys
+import time
+import threading
+
+exes = [
+  'all_gather_perf',
+  'all_reduce_perf',
+  'alltoall_perf',
+  'broadcast_perf',
+  'reduce_perf',
+  'reduce_scatter_perf',
+  'sendrecv_perf'
+]
+
+time_begin = time.time()
+exit_code = 0
+
+def env(x, deft=None):
+  s = os.environ.get(x, deft)
+  return s if deft is None else type(deft)(s)
+
+def subproc(args, env=None):
+  import subprocess as sp
+  import shlex
+  args = list(map(str, args))
+  env = {x:str(y) for x,y in (env or os.environ).items()}
+  p = sp.Popen(args, stdout=sp.PIPE, env=env)
+  try:
+    out, err = p.communicate(timeout=5*60)
+  except sp.TimeoutExpired:
+    exit_code = 1
+    print('** FAILED ** command (timeout > 5min):\n' + ' '.join(map(shlex.quote, args)), file=sys.stderr)
+    return None
+
+  if p.returncode != 0:
+    exit_code = p.returncode
+    print('** FAILED ** command (return=%d):\n'%p.returncode + ' '.join(map(shlex.quote, args)), file=sys.stderr)
+    return None
+  else:
+    return out.decode('utf-8')
+
+LD_LIBRARY_PATH = env('LD_LIBRARY_PATH', '')
+
+def die():
+  print("Usage: perf_regression.py old=<build-dir> new=<build-dir> threshold=<percent, default=5> <args-to-perf-exe>...")
+  exit(1)
+
+if '-h' in sys.argv or '--help' in sys.argv[1:]:
+  die()
+
+kwnames = ['new','old','threshold']
+kwargs = {x[:x.index('=')]:x[x.index('=')+1:] for x in sys.argv[1:] if '=' in x and x[:x.index('=')] in kwnames}
+exe_args = [x for x in sys.argv[1:] if '=' not in x or x[:x.index('=')] not in kwnames]
+
+if 'new' not in kwargs: die()
+build_new = kwargs['new']
+if 'old' not in kwargs: die()
+build_old = kwargs['old']
+threshold = float(kwargs.get('threshold', 5))
+
+def run_perf_mpi(proc_n, args, env):
+  xenv = []
+  for x,y in env.items():
+    if x.startswith('NCCL_') or x.startswith('CUDA') or x in ('LD_LIBRARY_PATH',):
+      xenv += ['-x','%s=%s'%(x,y)]
+  import shlex
+  salloc = shlex.split(os.environ.get('SALLOC',''))
+  return subproc(salloc + ['mpirun','-np',proc_n] + xenv + args, env)
+
+def run_perf(part_kind, args, env, key_prefix, times):
+  if part_kind == 'intra_proc':
+    proc_n = 1
+    args = args + ['-g2']
+  elif part_kind == 'inter_proc':
+    proc_n = 2
+    args = args + ['-g1']
+  else:
+    assert 0
+
+  out = run_perf_mpi(proc_n, args + ['--out_of_place',0], env)
+
+  for ln in (out or '').split('\n'):
+    ln = ln.rstrip()
+    if ln.startswith('#'):
+      if ln.split()[:4] == ['#','size','count','type']:
+        cuts=[]
+        for i in range(1,len(ln)):
+          if ln[i-1] not in (' ','#') and ln[i]==' ':
+            cuts += [i]
+        cuts = [0] + cuts + [len(ln)]
+        def field(ln,i):
+          return ln[cuts[i]:cuts[i+1]].strip()
+    elif ln != '':
+      try:
+        size = int(field(ln,0))
+        dtype = field(ln,2)
+        redop = field(ln,3)
+        t = float(field(ln,9)) # in-place time
+        key = key_prefix + (size,dtype,redop)
+        times[key] = min(t, times.get(key, t))
+      except:
+        print('BAD line:'+ln)
+        raise
+
+def sweep(times):
+  env = dict(os.environ) # copy of env
+  for trial in range(2):
+    for part_kind in ['intra_proc','inter_proc']:
+      for exe in exes:
+        protos = ['LL','LL128','SIMPLE']
+        algos = ['RING','TREE']
+        if exe in ['alltoall_perf','sendrecv_perf']:
+          protos = ['SIMPLE']
+          algos = ['RING']
+        elif exe in ['all_gather_perf','broadcast_perf','reduce_perf','reduce_scatter_perf']:
+          algos = ['RING']
+
+        if exe in ['all_reduce_perf','reduce_perf','reduce_scatter_perf']:
+          ops = [['-o','all', '-d','all']]
+        else:
+          ops = [['-d','int8']]
+
+        if part_kind == 'intra_proc':
+          protos = ['SIMPLE']
+          algos = ['RING']
+
+        if part_kind != 'inter_proc':
+          ops = [['-o','min', '-d','int8'],
+                 ['-o','max', '-d','half'],
+                 ['-o','sum', '-d','float'],
+                 ['-o','sum', '-d','int64']]
+        #protos = ['LL']
+        #algos = ['TREE']
+        #ops = ['-d','int8']
+
+        for proto in protos:
+          sizes = {
+            'LL':     ['-b64',  '-e64K', '-f1024', '-n500'],
+            'LL128':  ['-b256K','-e256M','-f32'],
+            'SIMPLE': ['-b32M', '-e1G',  '-f32']
+          }[proto]
+          for algo in algos:
+            for op in ops:
+              env['NCCL_PROTO'] = proto
+              env['NCCL_ALGO'] = algo
+              # run NEW test with old libnccl
+              print('Running part=%s trial=%d proto=%s algo=%s build=%s %s'%(part_kind,trial,proto,algo,build_old,' '.join(map(str,[exe]+sizes+op+exe_args))))
+              env['LD_LIBRARY_PATH'] = build_old+'/lib:'+LD_LIBRARY_PATH
+              run_perf(part_kind, [build_new+'/test/perf/'+exe] + sizes + op + exe_args, env, ('old',exe,part_kind,proto,algo), times)
+              # run new test with new libnccl
+              print('Running part=%s trial=%d proto=%s algo=%s build=%s %s'%(part_kind,trial,proto,algo,build_new,' '.join(map(str,[exe]+sizes+op+exe_args))))
+              env['LD_LIBRARY_PATH'] = build_new+'/lib:'+LD_LIBRARY_PATH
+              run_perf(part_kind, [build_new+'/test/perf/'+exe] + sizes + op + exe_args, env, ('new',exe,part_kind,proto,algo), times)
+
+times = {}
+sweep(times)
+
+bads=[]
+for (oldnew,exe,part_kind,proto,algo,size,dtype,redop),t0 in times.items():
+  if oldnew == 'old':
+    t1 = times['new',exe,part_kind,proto,algo,size,dtype,redop]
+    gain = 100*(t1 - t0)/t0
+    if gain >= threshold:
+      bads += [(gain,exe,part_kind,proto,algo,size,dtype,redop)]
+bads.sort(reverse=True)
+
+time_end = time.time()
+
+print("Elapsed seconds : %.2f"%(time_end-time_begin))
+
+def format_bytes(n):
+  if n < 1<<10:
+    return "%gB"%n
+  elif n < 1<<20:
+    return "%.3gK"%(n/(1<<10))
+  elif n < 1<<30:
+    return "%.3gM"%(n/(1<<20))
+  else:
+    return "%.3gG"%(n/(1<<30))
+
+if len(bads) > 0:
+  print("FAILURE cases, where time increased >= %.2f%%:"%threshold)
+  for gain,exe,part_kind,proto,algo,size,dtype,redop in bads:
+    print('%s %s %s %s %s %s %s : +%.2f%%'%(exe,part_kind,proto,algo,format_bytes(size),dtype,redop,gain))
+  if exit_code == 0:
+    exit_code = 1
+
+if exit_code == 0:
+  print("SUCCESS")
+exit(exit_code)
