e20b15a120aa1b35ab4eeb63733fb6766c0c7488
Sylvain Jeaugey <sjeaugey@nvidia.com>
2021-05-25 11:13:46 -0700

    Graph fixes for ZionEX
    Bump NVLink speed to 22.0 to help ZionEX tree search find 12x22
    instead of 11x24.
    
    Separate speed arrays and intra and inter-node to avoid intra-node
    DGX A100 finding 11x24 instead of 12x22.

--- src/graph/topo.h
+++ src/graph/topo.h
@@ -13,8 +13,8 @@
 
 #define LOC_WIDTH 5000.0
 #define SM60_NVLINK_WIDTH 18.0
-#define SM70_NVLINK_WIDTH 21.0
-#define SM80_NVLINK_WIDTH 21.0
+#define SM70_NVLINK_WIDTH 22.0
+#define SM80_NVLINK_WIDTH 22.0
 #define SM86_NVLINK_WIDTH 12.0
 #define PCI_WIDTH 12.0           // PCI Gen3 x16
 #define QPI_WIDTH 6.0
