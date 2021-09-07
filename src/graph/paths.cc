ab9e285cb036785fa13d274e9c6583b9c749b91a
Sylvain Jeaugey <sjeaugey@nvidia.com>
2021-09-07 06:59:45 -0700

    Disable PXN to check perf regression

--- src/graph/paths.cc
+++ src/graph/paths.cc
@@ -378,7 +378,7 @@ ncclResult_t ncclTopoGetIntermediateRank(struct ncclTopoSystem* system, int rank
   return ncclSuccess;
 }
 
-NCCL_PARAM(PxnDisable, "PXN_DISABLE", 0);
+NCCL_PARAM(PxnDisable, "PXN_DISABLE", 1);
 
 ncclResult_t ncclTopoComputePaths(struct ncclTopoSystem* system, struct ncclPeerInfo* peerInfos) {
   // Precompute paths between GPUs/NICs.
