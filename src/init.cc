bdfbd8849fa9dedd1b96e6c8b05c20ffc5bdd82c
Sylvain Jeaugey <sjeaugey@nvidia.com>
2021-12-16 06:14:51 -0800

    Fix crash in p2p when using imbalanced number of GPUs per node

--- src/init.cc
+++ src/init.cc
@@ -897,14 +897,18 @@ collnet_cleanup:
     free(nvbPeers);
   }
 
-  // Connect net proxies when using PXN path
+  // Connect to local net proxy
+  struct ncclProxyConnector proxyConn;
+  NCCLCHECK(ncclTopoGetLocalRank(comm->topo, comm->rank, &proxyConn.localRank));
+  NCCLCHECK(ncclProxyConnect(comm, TRANSPORT_NET, 1, comm->rank, &proxyConn));
+  NCCLCHECK(ncclProxyCall(&proxyConn, ncclProxyMsgSharedInit, &comm->p2pnChannels, sizeof(int), NULL, 0));
+
+  // Then to remote ones when using PXN
   if (ncclParamPxnDisable() == 0) {
     int nranks;
     int* pxnPeers;
     NCCLCHECK(ncclTopoGetPxnRanks(comm, &pxnPeers, &nranks));
     for (int r=0; r<nranks; r++) {
-      struct ncclProxyConnector proxyConn;
-      NCCLCHECK(ncclTopoGetLocalRank(comm->topo, comm->rank, &proxyConn.localRank));
       NCCLCHECK(ncclProxyConnect(comm, TRANSPORT_NET, 1, pxnPeers[r], &proxyConn));
       NCCLCHECK(ncclProxyCall(&proxyConn, ncclProxyMsgSharedInit, &comm->p2pnChannels, sizeof(int), NULL, 0));
     }
@@ -937,7 +941,9 @@ collnet_cleanup:
   /* Local intra-node barrier */
   NCCLCHECK(bootstrapBarrier(comm->bootstrap, comm->localRankToRank, comm->localRank, comm->localRanks, comm->localRankToRank[0]));
 
+  // Unlink proxy shm to make sure it will be properly cleaned up.
   NCCLCHECK(ncclProxyShmUnlink(comm));
+
   // We should have allocated all buffers, collective fifos, ... we can
   // restore the affinity.
 affinity_restore:
