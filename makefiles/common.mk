dd11d81d944cbe517c9f1995fb749bfab3f1fa10
Sylvain Jeaugey <sjeaugey@nvidia.com>
2022-01-07 06:26:43 -0800

    Move lsb_release call out of common.mk
    This generates "lsb_release not found" output on redhat builds.

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -23,8 +23,6 @@ CUDA_MAJOR = $(shell echo $(CUDA_VERSION) | cut -d "." -f 1)
 CUDA_MINOR = $(shell echo $(CUDA_VERSION) | cut -d "." -f 2)
 #$(info CUDA_VERSION ${CUDA_MAJOR}.${CUDA_MINOR})
 
-LSB_RELEASE=$(shell lsb_release -is)
-
 # You should define NVCC_GENCODE in your environment to the minimal set
 # of archs to reduce compile time.
 CUDA8_GENCODE = -gencode=arch=compute_35,code=sm_35 \
