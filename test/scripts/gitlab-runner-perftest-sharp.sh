04db8524cea12a113bc77f90322db148a6682451
Sylvain Jeaugey <sjeaugey@nvidia.com>
2022-03-02 05:54:14 -0800

    Fix sharp test script

--- test/scripts/gitlab-runner-perftest-sharp.sh
+++ test/scripts/gitlab-runner-perftest-sharp.sh
@@ -13,11 +13,9 @@ if [ "$graph" == "" ]; then graph=0; fi
 opts="-n 5 -w 1 -G $graph"
 range="-b 8 -e $max -f 2"
 
-if [ "$collnet" == "1" ]; then
-  export LD_LIBRARY_PATH=$SHARP_HOME/lib:$LD_LIBRARY_PATH
-  export LD_LIBRARY_PATH=$PLUGIN_PATH:$LD_LIBRARY_PATH
-  export NCCL_COLLNET_ENABLE=1
-  export NCCL_ALGO=COLLNET
-  echo "=============================== all_reduce (CollNet) ================================="
-  $SALLOC $MPI_HOME/bin/mpirun ./build/test/perf/all_reduce_perf $range $opts
-fi
+export LD_LIBRARY_PATH=$SHARP_HOME/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PLUGIN_PATH:$LD_LIBRARY_PATH
+export NCCL_COLLNET_ENABLE=1
+export NCCL_ALGO=COLLNET
+echo "=============================== all_reduce (CollNet) ================================="
+$SALLOC $MPI_HOME/bin/mpirun ./build/test/perf/all_reduce_perf $range $opts
