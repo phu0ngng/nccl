eaec511584f149871fffac7123ddc016426ce25f
Ben Williams <bewilliams@nvidia.com>
2022-05-10 11:55:46 -0700

    Re-added unit_tests needs (now that I understand how that works.) Checked errors in scripts and exit with a negative value if script fails. Trying to get SHARP to work

--- test/scripts/gitlab-runner-apitest.sh
+++ test/scripts/gitlab-runner-apitest.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$PWD/build/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
+# We need to catch failures manually and then throw at the end to get gitlab to detect a failure
+failure_count = 0
+
 echo "=============================== API TESTS Default ================================="
 $SRUN ./build/test/apitest/apitest
-if [ "$1" == "minimal" ]; then exit 0; fi
+status = $?
+[ $status -neq 0 ] && failure_count=$($failure_count + 1)
+
+if [ "$1" == "minimal" ]; then exit $failure_count; fi
 
 echo "=============================== API TESTS No P2P  ================================="
 NCCL_P2P_DISABLE=1 $SRUN ./build/test/apitest/apitest
+status = $?
+[ $status -neq 0 ] && failure_count=$($failure_count + 1)
 
 echo "=============================== API TESTS Network ================================="
 NCCL_SHM_DISABLE=1 NCCL_P2P_DISABLE=1 $SRUN ./build/test/apitest/apitest
+status = $?
+[ $status -neq 0 ] && failure_count=$($failure_count + 1)
+
+exit $failure_count
--- test/scripts/gitlab-runner-perftest-sharp.sh
+++ test/scripts/gitlab-runner-perftest-sharp.sh
@@ -17,5 +17,13 @@ export LD_LIBRARY_PATH=$SHARP_HOME/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$PLUGIN_PATH:$LD_LIBRARY_PATH
 export NCCL_COLLNET_ENABLE=1
 export NCCL_ALGO=COLLNET
+
+# We need to catch failures manually and then throw at the end to get gitlab to detect a failure
+failure_count = 0
+
 echo "=============================== all_reduce (CollNet) ================================="
 $SALLOC $MPI_HOME/bin/mpirun ./build/test/perf/all_reduce_perf $range $opts
+status = $?
+[ $status -neq 0 ] && failure_count=$($failure_count + 1)
+
+exit $failure_count
