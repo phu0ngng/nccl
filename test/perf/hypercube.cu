50262c3a16ed6bf30c70f35553110e7abf4c4571
Benjamin Williams <bewilliams@nvidia.com>
2022-05-14 08:27:30 -0700

    Don't run hypercube perf test on non-powers of 2 ranks

--- test/perf/hypercube.cu
+++ test/perf/hypercube.cu
@@ -97,9 +97,16 @@ testResult_t HyperCubeRunTest(struct threadArgs* args, int root, ncclDataType_t 
     run_typenames = test_typenames;
   }
 
-  for (int i=0; i<type_count; i++) {
-    TESTCHECK(TimeTest(args, run_types[i], run_typenames[i], (ncclRedOp_t)0, "", -1));
+  // Check if this is a power of 2
+  int nRanks = args->nProcs*args->nThreads*args->nGpus;
+  if (nRanks && !(nRanks & (nRanks - 1))) {
+    for (int i=0; i<type_count; i++) {
+      TESTCHECK(TimeTest(args, run_types[i], run_typenames[i], (ncclRedOp_t)0, "", -1));
+    }
+  } else {
+    printf("nRanks %d is not a power of 2, skipping\n", nRanks);
   }
+
   return testSuccess;
 }
 
