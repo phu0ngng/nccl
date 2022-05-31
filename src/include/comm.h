fdde039bf7043e4f1411eb0f54fb1464d871516a
John Bachan <jbachan@nvidia.com>
2022-05-31 08:41:59 -0700

    Comment about expected use of

--- src/include/comm.h
+++ src/include/comm.h
@@ -278,6 +278,9 @@ struct ncclComm {
   struct ncclKernelPlan* unlaunchedPlansHead;
 };
 
+// Set to true during an `atexit()` handler. We use this to intentionally leak
+// unfreed CUDA resources when cleaning up after return of `main()` to avoid
+// CUDA calls after CUDA runtime teardown.
 extern bool ncclMainExited;
 
 enum ncclLaunchMode {
