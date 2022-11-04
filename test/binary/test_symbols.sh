2ee776663e84d7436fda71a678a00819ddf10c81
Sylvain Jeaugey <sjeaugey@nvidia.com>
2022-11-04 07:53:36 -0700

    Fix binary test and pncclGetLastError definition
    Improve binary test to make sure we have a pnccl symbol for each nccl
    symbol.
    Fix the pncclGetLastError definition so that pncclGetLastError is
    exported as "C" symbol.
    
    Bug 3856510

--- test/binary/test_symbols.sh
+++ test/binary/test_symbols.sh
@@ -5,22 +5,43 @@ if [ "$lib" == "" ]; then
   lib=`dirname $0`/../../lib/libnccl.so
 fi
 
-exportedsymbols=`nm --dynamic --defined-only $lib | cut -c 20-`
+ncclsymbols=`nm --dynamic --defined-only $lib | cut -c 20- | grep "^nccl"`
+pncclsymbols=`nm --dynamic --defined-only $lib | cut -c 20- | grep "^pnccl"`
+othersymbols=`nm --dynamic --defined-only $lib | cut -c 20- | grep -v "^nccl" | grep -v "^pnccl"`
 errors=""
 
 sym_ok() {
-s=$1
+  s=$1
   othersymbols="_init _fini __bss_start _end _edata"
   for sym in $othersymbols; do
     if [ "$s" == "$sym" ]; then
       return
     fi
   done
-  echo $symbol | grep -v "^nccl"
+  echo $symbol
 }
 
+while [ "$ncclsymbols" != "" ]; do
+  set -- $ncclsymbols
+  ncclsym=$1
+  shift
+  ncclsymbols=$@
+  set -- $pncclsymbols
+  found=0
+  for pncclsym in $pncclsymbols; do
+    if [ "`echo $pncclsym | cut -c 2-`" == "$ncclsym" ]; then
+      found=1
+      break
+    fi
+  done
+  if [ "$found" == "1" ]; then
+    echo -e "\e[32m\e[1m  [OK]\e[0m\t $ncclsym p$ncclsym"
+  else
+    echo -e "\e[31m\e[1m [FAIL]\e[0m\t $ncclsym"
+  fi
+done
 
-for symbol in $exportedsymbols; do
+for symbol in $othersymbols; do
   sym=`sym_ok $symbol`
   if [ "$sym" == "" ]; then
     echo -e "\e[32m\e[1m  [OK]\e[0m\t $symbol"
