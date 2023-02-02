e0d8a2d915c2a534990fedbecaa6bf62a6ad61e7
John Bachan <jbachan@nvidia.com>
2023-02-01 16:42:31 -0800

    Another fix to compilation of device code on sm90 with CUDA<12.1

--- src/collectives/device/prims_simple.h
+++ src/collectives/device/prims_simple.h
@@ -77,7 +77,9 @@ class Primitives<
     uintptr_t addr = cvta_to_global(ptr);
     uint64_t ans;
     if (MC && (flags & McMinPolling)) {
-      asm("multimem.ld_reduce.global.min.u64 %0, [%1];" : "=l"(ans) : "l"(addr));
+      #if __CUDA_ARCH__ >= 900 && CUDART_VERSION >= 12010
+        asm("multimem.ld_reduce.global.min.u64 %0, [%1];" : "=l"(ans) : "l"(addr));
+      #endif
     } else {
       asm("ld.volatile.global.u64 %0, [%1];": "=l"(ans) : "l"(addr));
     }
