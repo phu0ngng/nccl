23222a63fa3e272eec707beeb658e666458c812a
David Addison <daddison@nvidia.com>
2023-02-02 14:30:34 -0800

    Updated Copyright to 2023 for newly added files

--- src/include/ipcsocket.h
+++ src/include/ipcsocket.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2022, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2016-2023, NVIDIA CORPORATION. All rights reserved.
  *
  * See COPYRIGHT for license information
  */
