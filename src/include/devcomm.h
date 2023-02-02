85c6280fbc10560494f875f944c420a8257419fb
John Bachan <jbachan@nvidia.com>
2023-02-02 14:18:33 -0800

    Compilation error fix to devcomm.h

--- src/include/devcomm.h
+++ src/include/devcomm.h
@@ -350,7 +350,7 @@ __host__ __device__ constexpr int ncclShmemScratchWarpSize(int cudaArch = NCCL_C
   return (max_constexpr<int>(
       /*LL    */0,
       /*LL128 */(NCCL_LL128_SHMEM_ELEMS_PER_THREAD*WARP_SIZE)*sizeof(uint64_t),
-      /*SIMPLE*/(ncclCollUnroll(cudaArch)*WARP_SIZE + 1)*16
+      /*SIMPLE*/(ncclCollUnroll(cudaArch)*WARP_SIZE + 1)*16,
       // MC needs an extra 16B to read unaligned data.
       /*MC    */WARP_SIZE*(cudaArch >= 900 ? ncclMCUnrollBytes(cudaArch) : 0) + 16
     ) + 15) & -16; // pad to 16 bytes
