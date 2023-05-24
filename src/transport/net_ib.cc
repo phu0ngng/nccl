d8c3b21003094bb87a7e9828c917841176bd646b
Sylvain Jeaugey <sjeaugey@nvidia.com>
2023-05-24 12:27:49 -0700

    Extend VF merging to two digits.
    We used to merge device ABCD:EF:GX.Y with ABCD:EF:G0.0, but on some
    systems, VFs can go beyond the first digit, e.g. 0000:89:10.0.
    
    So we need to also zero the 4th char from the end to attach VFs to
    the right PCI device, changing ABCD:EF:XY.Z to ABCD:EF:00.0.

--- src/transport/net_ib.cc
+++ src/transport/net_ib.cc
@@ -111,7 +111,7 @@ static ncclResult_t ncclIbGetPciPath(char* devName, char** path, int* realPort) 
     // Merge multi-port NICs into the same PCI device
     p[strlen(p)-1] = '0';
     // Also merge virtual functions (VF) into the same device
-    if (ncclParamIbMergeVfs()) p[strlen(p)-3] = '0';
+    if (ncclParamIbMergeVfs()) p[strlen(p)-3] = p[strlen(p)-4] = '0';
     // And keep the real port aside (the ibv port is always 1 on recent cards)
     *realPort = 0;
     for (int d=0; d<ncclNIbDevs; d++) {
