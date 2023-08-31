0e1a765cc4bcc29a565a5842b50189b8f659fca9
Sylvain Jeaugey <sjeaugey@nvidia.com>
2023-08-31 03:34:52 -0700

    Better tuning for NVLSTree on 4+ nodes

--- src/enqueue.cc
+++ src/enqueue.cc
@@ -1347,6 +1347,7 @@ comp_next:
   } else if (info->algorithm == NCCL_ALGO_NVLS_TREE) {
     // Use uint64_t so that concurrentOps*chunkSize*X does not overflow
     uint64_t concurrentOps = info->nChannels*info->comm->channels[0].nvls.nHeads;
+    if (info->comm->nNodes >= 4) chunkSize = 65536;
     if ((info->nBytes < (32 * (concurrentOps*chunkSize))) && (chunkSize > 262144)) chunkSize = 262144;
     if ((info->nBytes < (16 * (concurrentOps*chunkSize))) && (chunkSize > 131072)) chunkSize = 131072;
     if ((info->nBytes < (4 * (concurrentOps*chunkSize))) && (chunkSize > 65536)) chunkSize = 65536;
