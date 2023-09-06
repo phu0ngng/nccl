fd622e1fa957dbed6f077e5516717c587f6f5299
Sylvain Jeaugey <sjeaugey@nvidia.com>
2023-09-06 07:53:03 -0700

    Better tuning for NVLS+Collnet on H800 and 2 nodes

--- src/enqueue.cc
+++ src/enqueue.cc
@@ -1337,6 +1337,7 @@ comp_next:
     work->lastChunkSize = chunkSize / ncclTypeSize(info->datatype);
   } else if (info->algorithm == NCCL_ALGO_NVLS) {
     int maxChunkSize = 131072;
+    if (info->comm->nNodes > 1 && info->comm->bandwidths[ncclFuncAllReduce][NCCL_ALGO_NVLS][NCCL_PROTO_SIMPLE] < 150) maxChunkSize = 32768;
     if (chunkSize > maxChunkSize) chunkSize = maxChunkSize;
     // Use uint64_t so that concurrentOps*chunkSize*X does not overflow
     uint64_t concurrentOps = info->nChannels*info->comm->channels[0].nvls.nHeads;
