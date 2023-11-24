2c9494c889b2abfa738ee65dad1da9719ea25ac5
Sylvain Jeaugey <sjeaugey@nvidia.com>
2023-11-24 02:24:36 -0800

    Move to merged register file

--- src/register.cc
+++ src/register.cc
@@ -8,16 +8,7 @@
 #include "nccl.h"
 #include "comm.h"
 #include "net.h"
-
-struct ncclReg {
-  uintptr_t addr;
-  size_t pages;
-  int refs;
-  int nComms;
-  void** sComms;
-  void** rComms;
-  void** handles;
-};
+#include "register.h"
 
 ncclResult_t ncclNetDeregister(struct ncclComm* comm, struct ncclReg* reg) {
   ncclDebugNoWarn = NCCL_NET;
@@ -35,98 +26,95 @@ ncclResult_t ncclNetDeregister(struct ncclComm* comm, struct ncclReg* reg) {
   return ncclSuccess;
 }
 
-ncclResult_t ncclNetRegister(struct ncclComm* comm, void* addr, size_t size, struct ncclReg* reg) {
-  int netDevs;
-  NCCLCHECK(comm->ncclNet->devices(&netDevs));
+// ncclResult_t ncclNetRegister(struct ncclComm* comm, void* addr, size_t size, struct ncclReg* reg) {
+//   int netDevs;
+//   NCCLCHECK(comm->ncclNet->devices(&netDevs));
 
-  int localNetDevCount = 0;
-  int* localNetDevs;
-  void *lComm = NULL;
-  ncclResult_t ret = ncclSuccess;
+//   int localNetDevCount = 0;
+//   int* localNetDevs;
+//   void *lComm = NULL;
+//   ncclResult_t ret = ncclSuccess;
 
-  NCCLCHECK(ncclCalloc(&localNetDevs, comm->p2pnChannels));
+//   NCCLCHECK(ncclCalloc(&localNetDevs, comm->p2pnChannels));
 
-  // Find local devices for p2p operations
-  for (int c=0; c<comm->p2pnChannels; c++) {
-    int dev;
-    if (ncclTopoGetLocalNet(comm->topo, comm->rank, c, &dev) != ncclSuccess) goto end; // No local net
-    ncclNetProperties_t props;
-    NCCLCHECKGOTO(comm->ncclNet->getProperties(dev, &props), ret, end);
-    if (props.regIsGlobal == 0) { // We need to be sure all NICs support global registration.
-      localNetDevCount = 0;
-      break;
-    }
-    localNetDevs[localNetDevCount++] = dev;
-  }
+//   // Find local devices for p2p operations
+//   for (int c=0; c<comm->p2pnChannels; c++) {
+//     int dev;
+//     if (ncclTopoGetLocalNet(comm->topo, comm->rank, c, &dev) != ncclSuccess) goto end; // No local net
+//     ncclNetProperties_t props;
+//     NCCLCHECKGOTO(comm->ncclNet->getProperties(dev, &props), ret, end);
+//     if (props.regIsGlobal == 0) { // We need to be sure all NICs support global registration.
+//       localNetDevCount = 0;
+//       break;
+//     }
+//     localNetDevs[localNetDevCount++] = dev;
+//   }
 
-  NCCLCHECKGOTO(ncclCalloc(&reg->sComms, localNetDevCount), ret, end);
-  NCCLCHECKGOTO(ncclCalloc(&reg->rComms, localNetDevCount), ret, end);
-  NCCLCHECKGOTO(ncclCalloc(&reg->handles, localNetDevCount), ret, end);
-  reg->nComms = localNetDevCount;
+//   NCCLCHECKGOTO(ncclCalloc(&reg->sComms, localNetDevCount), ret, end);
+//   NCCLCHECKGOTO(ncclCalloc(&reg->rComms, localNetDevCount), ret, end);
+//   NCCLCHECKGOTO(ncclCalloc(&reg->handles, localNetDevCount), ret, end);
+//   reg->nComms = localNetDevCount;
 
-  ncclDebugNoWarn = NCCL_NET;
-  for (int d=0; d<localNetDevCount; d++) {
-    int dev = localNetDevs[d];
-    reg->handles[d] = reg->sComms[d] = reg->rComms[d] = NULL;
+//   ncclDebugNoWarn = NCCL_NET;
+//   for (int d=0; d<localNetDevCount; d++) {
+//     int dev = localNetDevs[d];
+//     reg->handles[d] = reg->sComms[d] = reg->rComms[d] = NULL;
 
-    ncclNetHandle_t netHandle;
-    NCCLCHECKGOTO(comm->ncclNet->listen(dev, &netHandle, &lComm), ret, end);
+//     ncclNetHandle_t netHandle;
+//     NCCLCHECKGOTO(comm->ncclNet->listen(dev, &netHandle, &lComm), ret, end);
 
-    bool connected;
-    connected = false;
-    while (!connected) {
-      if (*comm->abortFlag) {
-        goto end;
-      }
+//     bool connected;
+//     connected = false;
+//     while (!connected) {
+//       if (*comm->abortFlag) {
+//         goto end;
+//       }
 
-      if (reg->sComms[d] == NULL)
-        NCCLCHECKGOTO(comm->ncclNet->connect(dev, &netHandle, reg->sComms+d, NULL), ret, end);
+//       if (reg->sComms[d] == NULL)
+//         NCCLCHECKGOTO(comm->ncclNet->connect(dev, &netHandle, reg->sComms+d), ret, end);
 
-      if (reg->rComms[d] == NULL)
-        NCCLCHECKGOTO(comm->ncclNet->accept(lComm, reg->rComms+d, NULL), ret, end);
+//       if (reg->rComms[d] == NULL)
+//         NCCLCHECKGOTO(comm->ncclNet->accept(lComm, reg->rComms+d), ret, end);
 
-      connected = (reg->rComms[d] != NULL) && (reg->sComms[d] != NULL);
-    }
-    NCCLCHECK(comm->ncclNet->closeListen(lComm));
-    lComm = NULL;
+//       connected = (reg->rComms[d] != NULL) && (reg->sComms[d] != NULL);
+//     }
+//     NCCLCHECK(comm->ncclNet->closeListen(lComm));
+//     lComm = NULL;
 
-    if (comm->ncclNet->regMr(reg->sComms[d], addr, size, NCCL_PTR_CUDA, reg->handles+d) != ncclSuccess) {
-      reg->handles[d] = NULL;
-    }
-  }
-end:
-  ncclDebugNoWarn = 0;
-  if (ret != ncclSuccess) NCCLCHECK(ncclNetDeregister(comm, reg));
-  free(localNetDevs);
-  return ret;
-}
+//     if (comm->ncclNet->regMr(reg->sComms[d], addr, size, NCCL_PTR_CUDA, reg->handles+d) != ncclSuccess) {
+//       reg->handles[d] = NULL;
+//     }
+//   }
+// end:
+//   ncclDebugNoWarn = 0;
+//   if (ret != ncclSuccess) NCCLCHECK(ncclNetDeregister(comm, reg));
+//   free(localNetDevs);
+//   return ret;
+// }
 
-ncclResult_t ncclRegFind(struct ncclComm* comm, void* data, size_t size, int* found) {
-  static __thread uintptr_t pageSize = 0;
-  if (pageSize == 0) pageSize = sysconf(_SC_PAGESIZE);
-  *found = 0;
-
+ncclResult_t ncclRegFind(struct ncclComm* comm, const void* data, size_t size, struct ncclReg** reg) {
   struct ncclRegCache* cache = &comm->regCache;
+  uintptr_t pageSize = cache->pageSize;
   uintptr_t addr = (uintptr_t)data & -pageSize;
   size_t pages = ((uintptr_t)data + size - addr + pageSize-1)/pageSize;
 
+  *reg = NULL;
   for (int slot=0; /*true*/; slot++) {
     if (slot == cache->population || addr < cache->slots[slot]->addr) return ncclSuccess;
     if ((addr >= cache->slots[slot]->addr) &&
         ((addr-cache->slots[slot]->addr)/pageSize+pages) <= cache->slots[slot]->pages) {
-      *found = cache->slots[slot]->nComms ? 1 : 0;
+      *reg = cache->slots[slot];
       return ncclSuccess;
     }
   }
 }
-NCCL_PARAM(RegDisable, "REGISTER_DISABLE", 0);
+// NCCL_PARAM(RegDisable, "REGISTER_DISABLE", 0);
+NCCL_PARAM(LocalRegister, "LOCAL_REGISTER", 1);
 
 ncclResult_t ncclRegister(struct ncclComm* comm, void* data, size_t size, void** handle) {
-  if (ncclParamRegDisable()) return ncclSuccess;
-  static __thread uintptr_t pageSize = 0;
-  if (pageSize == 0) pageSize = sysconf(_SC_PAGESIZE);
-
+  if (!ncclParamLocalRegister()) return ncclSuccess;
   struct ncclRegCache* cache = &comm->regCache;
+  uintptr_t pageSize = cache->pageSize;
   uintptr_t addr = (uintptr_t)data & -pageSize;
   size_t pages = ((uintptr_t)data + size - addr + pageSize-1)/pageSize;
   for (int slot=0; /*true*/; slot++) {
@@ -141,7 +129,8 @@ ncclResult_t ncclRegister(struct ncclComm* comm, void* data, size_t size, void**
       regSlot->addr = addr;
       regSlot->pages = pages;
       regSlot->refs = 1;
-      NCCLCHECK(ncclNetRegister(comm, (void*)addr, pages*pageSize, regSlot));
+      // NCCLCHECK(ncclNetRegister(comm, (void*)addr, pages*pageSize, regSlot));
+      regSlot->state |= NET_REG_COMPLETE;
       cache->population += 1;
       *handle = regSlot;
       return ncclSuccess;
@@ -158,239 +147,38 @@ ncclResult_t ncclRegCleanup(struct ncclComm* comm) {
   struct ncclRegCache* cache = &comm->regCache;
   for (int i=0; i<cache->population; i++) {
     INFO(NCCL_INIT, "Cleanup buffer %p pages %lx", (void*)cache->slots[i]->addr, cache->slots[i]->pages);
-    NCCLCHECK(ncclNetDeregister(comm, cache->slots[i]));
+    // NCCLCHECK(ncclNetDeregister(comm, cache->slots[i]));
+    NCCLCHECK(ncclNvlsDeregBuffer(&cache->slots[i]->mcHandle, cache->slots[i]->regAddr, cache->slots[i]->dev, cache->slots[i]->regSize));
     free(cache->slots[i]);
   }
   free(cache->slots);
   return ncclSuccess;
 }
 
-NCCL_PARAM(LocalRegister, "LOCAL_REGISTER", 1);
-NCCL_PARAM(LocalRegisterNet, "LOCAL_REGISTER_NET", 1);
-NCCL_PARAM(LocalRegisterNvls, "LOCAL_REGISTER_NVLS", 1);
-
 NCCL_API(ncclResult_t, ncclCommRegister, const ncclComm_t comm, void* buff, size_t size, void** handle);
 ncclResult_t ncclCommRegister(const ncclComm_t comm, void* buff, size_t size, void** handle) {
-  NVTX3_FUNC_RANGE_IN(nccl_domain);
-  ncclResult_t ret = ncclSuccess;
-
-  if (ncclParamLocalRegister()) {
-    if (comm == NCCL_COMM_NULL || buff == NULL || handle == NULL || size == 0) {
-      WARN("Invalid arguments comm %p, buff %p, size %ld, handle %p", comm, buff, size, handle);
-      ret = ncclInvalidArgument;
-    } else {
-      if (ncclParamLocalRegisterNet()) {
-        NCCLCHECK(ncclRegister(comm, buff, size, handle));
-      }
-#if CUDART_VERSION >= 12010
-      if (comm->nvlsSupport && ncclParamLocalRegisterNvls()) {
-        size_t granularity;
-        CUmulticastObjectProp prop = comm->nvlsResources->properties;
-
-        prop.size = size;
-        CUCHECK(cuMulticastGetGranularity(&granularity, &prop, CU_MULTICAST_GRANULARITY_RECOMMENDED));
-
-        if ((uintptr_t)buff % comm->nvlsResources->ucGran == 0 && size % granularity == 0) {
-          /* we can direct register what user provide */
-          struct ncclRegRequest* req;
-          NCCLCHECK(ncclCalloc(&req, 1));
-          req->buff = (uintptr_t)buff;
-          req->size = size;
-          ncclIntruQueueEnqueue(&comm->regRequestQueue, req);
-          *handle = (void*)req;
-        } else {
-          void* base;
-          size_t baseSize;
-          /* Since we don't provide actually allocated buffer size for users by ncclMemAlloc,
-           * therefore, we need to get the full range of the buffer by cuMemGetAddressRange to
-           * register buffers. */
-          CUCHECK(cuMemGetAddressRange((CUdeviceptr*)&base, &baseSize, (CUdeviceptr)buff));
-          if ((uintptr_t)base % comm->nvlsResources->ucGran == 0 && baseSize % granularity == 0) {
-            struct ncclRegRequest* req;
-            NCCLCHECK(ncclCalloc(&req, 1));
-            req->buff = (uintptr_t)base;
-            req->size = baseSize;
-            ncclIntruQueueEnqueue(&comm->regRequestQueue, req);
-            *handle = (void*)req;
-          } else {
-            WARN("register fails, buffer %p (aligned %s, granularity %ld) and size %ld (aligned %s, granularity %ld) for registration", buff, (uintptr_t)buff % comm->nvlsResources->ucGran == 0 ? "TRUE" : "FALSE", comm->nvlsResources->ucGran, size, size % granularity == 0 ? "TRUE" : "FALSE", granularity);
-            ret = ncclInvalidArgument;
-          }
-        }
-      }
-#endif
-    }
-  }
-
-  return ret;
+  NCCLCHECK(PtrCheck(comm, "ncclCommRegister", "comm"));
+  if (comm->checkPointers) NCCLCHECK(CudaPtrCheck(buff, comm, "buff", "ncclCommRegister"));
+  NCCLCHECK(ncclRegister(comm, buff, size, handle));
+  return ncclSuccess;
 }
 
 NCCL_API(ncclResult_t, ncclCommDeregister, const ncclComm_t comm, void* handle);
 ncclResult_t ncclCommDeregister(const ncclComm_t comm, void* handle) {
-  ncclResult_t ret = ncclSuccess;
-
-  struct ncclRegRequest* dreq = (struct ncclRegRequest*)handle;
-  if (ncclParamLocalRegister()) {
-    if (comm == NCCL_COMM_NULL || handle == NULL) {
-      WARN("Invalid arguments comm %p, handle %p", comm, handle);
-      ret = ncclInvalidArgument;
-    } else {
-      if (ncclParamLocalRegisterNet()) {
-        struct ncclReg* reg = (struct ncclReg*)handle;
-        struct ncclRegCache* cache = &comm->regCache;
-        int slot;
-        for (slot=0; slot<cache->population && cache->slots[slot] != reg; slot++);
-        if (slot == cache->population) {
-          WARN("Deregister: Could not find handle");
-          return ncclInvalidUsage;
-        }
-        if (--reg->refs) return ncclSuccess;
-        NCCLCHECK(ncclNetDeregister(comm, reg));
-        free(reg);
-        memmove(cache->slots+slot, cache->slots+slot+1, (cache->population-slot-1)*sizeof(struct ncclReg*));
-        cache->population -= 1;
-        return ncclSuccess;
-      }
-#if CUDART_VERSION >= 12010
-      if (comm->nvlsSupport && ncclParamLocalRegisterNvls()) {
-        struct ncclRegRecord* rec;
-
-        /* first release register record */
-        rec = ncclIntruQueueHead(&comm->regRecordQueue);
-
-        while (rec) {
-          if (rec->buff == dreq->buff && rec->size == dreq->size) {
-            NCCLCHECK(ncclNvlsDeregBuffer(&rec->mcHandle, rec->regAddr, rec->dev, rec->regSize));
-            ncclIntruQueueDelete(&comm->regRecordQueue, rec);
-            free(rec->addrs);
-            free(rec);
-            break;
-          }
-          rec = rec->next;
-        }
-
-        /* then free register request */
-        if (ncclIntruQueueDelete(&comm->regRequestQueue, dreq) == false) {
-          WARN("Invalid handle %p", handle);
-          ret = ncclInvalidArgument;
-        }
-      }
-#endif
-    }
+  NCCLCHECK(PtrCheck(comm, "ncclCommRegister", "comm"));
+  struct ncclReg* reg = (struct ncclReg*)handle;
+  struct ncclRegCache* cache = &comm->regCache;
+  int slot;
+  for (slot=0; slot<cache->population && cache->slots[slot] != reg; slot++);
+  if (slot == cache->population) {
+    WARN("Deregister: Could not find handle");
+    return ncclInvalidUsage;
   }
-
-  return ret;
-}
-
-NCCL_API(ncclResult_t, ncclMemAlloc, void **ptr, size_t size);
-ncclResult_t  ncclMemAlloc(void **ptr, size_t size) {
-  NVTX3_FUNC_RANGE_IN(nccl_domain);
-  ncclResult_t ret = ncclSuccess;
-
-#if CUDART_VERSION >= 12010
-  size_t memGran = 0;
-  size_t mcGran = 0;
-  CUdevice currentDev;
-  CUmemAllocationProp memprop = {};
-  CUmulticastObjectProp mcprop = {};
-  CUmemAccessDesc accessDesc = {};
-  CUmemGenericAllocationHandle handle;
-  int cudaDev;
-  int flag = 0;
-  int dcnt;
-  int mcSupport = 0;
-
-  if (ptr == NULL || size == 0) goto fallback;
-
-  if (ncclCudaLibraryInit() != ncclSuccess) goto fallback;
-
-  CUDACHECK(cudaGetDevice(&cudaDev));
-  CUCHECK(cuDeviceGet(&currentDev, cudaDev));
-  if (CUPFN(cuMulticastCreate) != NULL)
-    CUCHECK(cuDeviceGetAttribute(&mcSupport, CU_DEVICE_ATTRIBUTE_MULTICAST_SUPPORTED, currentDev));
-
-  if (mcSupport) {
-    memprop.type = CU_MEM_ALLOCATION_TYPE_PINNED;
-    memprop.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
-    memprop.requestedHandleTypes = NVLS_CU_MEM_HANDLE_TYPE;
-    memprop.location.id = currentDev;
-    // Query device to see if RDMA support is available
-    CUCHECK(cuDeviceGetAttribute(&flag, CU_DEVICE_ATTRIBUTE_GPU_DIRECT_RDMA_SUPPORTED, currentDev));
-    if (flag) memprop.allocFlags.gpuDirectRDMACapable = 1;
-    CUCHECK(cuMemGetAllocationGranularity(&memGran, &memprop, CU_MEM_ALLOC_GRANULARITY_RECOMMENDED));
-
-    /* mc property */
-    CUDACHECK(cudaGetDeviceCount(&dcnt));
-    mcprop.size = size;
-    /* device cnt is a dummy value right now, it might affect mc granularity in the future. */
-    mcprop.numDevices = dcnt;
-    mcprop.handleTypes = NVLS_CU_MEM_HANDLE_TYPE;
-    mcprop.flags = 0;
-    CUCHECK(cuMulticastGetGranularity(&mcGran, &mcprop, CU_MULTICAST_GRANULARITY_RECOMMENDED));
-
-    /* only size needs to be aligned to mcGran */
-    ALIGN_SIZE(size, mcGran);
-    /* Allocate the physical memory on the device */
-    CUCHECK(cuMemCreate(&handle, size, &memprop, 0));
-    /* Reserve a virtual address range */
-    CUCHECK(cuMemAddressReserve((CUdeviceptr*)ptr, size, memGran, 0, 0));
-    /* Map the virtual address range to the physical allocation */
-    CUCHECK(cuMemMap((CUdeviceptr)*ptr, size, 0, handle, 0));
-    /* Now allow RW access to the newly mapped memory */
-    for (int i = 0; i < dcnt; ++i) {
-      int p2p = 0;
-      if (i == cudaDev || ((cudaDeviceCanAccessPeer(&p2p, cudaDev, i) == cudaSuccess) && p2p)) {
-        accessDesc.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
-        accessDesc.location.id = i;
-        accessDesc.flags = CU_MEM_ACCESS_FLAGS_PROT_READWRITE;
-        CUCHECK(cuMemSetAccess((CUdeviceptr)*ptr, size, &accessDesc, 1));
-      }
-    }
-    goto exit;
-  }
-
-fallback:
-#endif
-  CUDACHECKGOTO(cudaMalloc(ptr, size), ret, fail);
-
-exit:
-  return ret;
-fail:
-  goto exit;
-}
-
-NCCL_API(ncclResult_t, ncclMemFree, void *ptr);
-ncclResult_t  ncclMemFree(void *ptr) {
-  NVTX3_FUNC_RANGE_IN(nccl_domain);
-  ncclResult_t ret = ncclSuccess;
-  int saveDevice;
-
-  CUDACHECK(cudaGetDevice(&saveDevice));
-#if CUDART_VERSION >= 12010
-  CUdevice ptrDev = 0;
-  int mcSupport = 0;
-
-  if (ptr == NULL) goto fallback;
-
-  if (ncclCudaLibraryInit() != ncclSuccess) goto fallback;
-
-  CUCHECKGOTO(cuPointerGetAttribute((void*)&ptrDev, CU_POINTER_ATTRIBUTE_DEVICE_ORDINAL, (CUdeviceptr)ptr), ret, fail);
-  if (CUPFN(cuMulticastCreate) != NULL)
-    CUCHECKGOTO(cuDeviceGetAttribute(&mcSupport, CU_DEVICE_ATTRIBUTE_MULTICAST_SUPPORTED, ptrDev), ret, fail);
-
-  CUDACHECKGOTO(cudaSetDevice((int)ptrDev), ret, fail);
-  if (mcSupport) {
-    NCCLCHECKGOTO(ncclCuMemFree(ptr), ret, fail);
-    goto exit;
-  }
-
-fallback:
-#endif
-  CUDACHECKGOTO(cudaFree(ptr), ret, fail);
-
-exit:
-  cudaSetDevice(saveDevice);
-  return ret;
-fail:
-  goto exit;
+  if (--reg->refs) return ncclSuccess;
+  // NCCLCHECK(ncclNetDeregister(comm, reg));
+  NCCLCHECK(ncclNvlsDeregBuffer(&reg->mcHandle, reg->regAddr, reg->dev, reg->regSize));
+  free(reg);
+  memmove(cache->slots+slot, cache->slots+slot+1, (cache->population-slot-1)*sizeof(struct ncclReg*));
+  cache->population -= 1;
+  return ncclSuccess;
 }
