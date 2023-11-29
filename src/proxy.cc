395a11b46faed08a35294760591be6f6d9a79c88
Sylvain Jeaugey <sjeaugey@nvidia.com>
2023-11-29 07:00:46 -0800

    Disable user buffer access when using the LL protocol

--- src/proxy.cc
+++ src/proxy.cc
@@ -615,7 +615,7 @@ ncclResult_t ncclProxyComputeP2p(struct ncclInfo* info, struct ncclProxyOp* op, 
       // Tune chunk size for the network
       if (info->count < stepSize) info->chunkSize /= 4;
       else if (info->count < 8*stepSize) info->chunkSize /= 2;
-      if (peer->send[1].proxyConn.sameProcess) op->reg = reg;
+      if (info->protocol == NCCL_PROTO_SIMPLE && peer->send[1].proxyConn.sameProcess) op->reg = reg;
     }
   } else if (info->coll == ncclFuncRecv) {
     op->pattern = ncclPatternRecv;
@@ -623,7 +623,7 @@ ncclResult_t ncclProxyComputeP2p(struct ncclInfo* info, struct ncclProxyOp* op, 
       // Tune chunk size for the network
       if (info->count < stepSize) info->chunkSize /= 4;
       else if (info->count < 8*stepSize) info->chunkSize /= 2;
-      if (peer->recv[1].proxyConn.sameProcess) op->reg = reg;
+      if (info->protocol == NCCL_PROTO_SIMPLE && peer->recv[1].proxyConn.sameProcess) op->reg = reg;
     }
   } else {
     WARN("P2p operation is neither send or recv");
