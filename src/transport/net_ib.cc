530c7decb5974fa4b0b31222c3933723b095bf79
Sylvain Jeaugey <sjeaugey@nvidia.com>
2024-01-22 04:05:08 -0800

    Set default QPs per connection back to 1

--- src/transport/net_ib.cc
+++ src/transport/net_ib.cc
@@ -632,7 +632,7 @@ struct ncclIbRecvComm {
 };
 static_assert((offsetof(struct ncclIbRecvComm, remFifo) % 32) == 0, "ncclIbRecvComm fifo must be 32-byte aligned");
 
-NCCL_PARAM(IbQpsPerConn, "IB_QPS_PER_CONNECTION", 2);
+NCCL_PARAM(IbQpsPerConn, "IB_QPS_PER_CONNECTION", 1);
 
 static void ncclIbAddEvent(struct ncclIbRequest* req, int devIndex, struct ncclIbNetCommDevBase* base) {
   req->events[devIndex]++;
