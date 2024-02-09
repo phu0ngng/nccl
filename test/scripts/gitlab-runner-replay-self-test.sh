781db3f894ccfb2bf912f164c71d0231baf7a3f9
Ben Williams <bewilliams@nvidia.com>
2024-02-09 07:02:06 -0800

    Replay runner script

--- test/scripts/gitlab-runner-replay-self-test.sh
+++ test/scripts/gitlab-runner-replay-self-test.sh
@@ -18,13 +18,19 @@ echo "Using SALLOC_SELF_SINGLE=$SALLOC_SELF_SINGLE"
 
 echo "=============================== Generating all_reduce trace file - $(date +\"%T\") ================================="
 $SALLOC_SELF_MULTI $MPI_HOME/bin/mpirun $MPI_PARAMS -x NCCL_DEBUG_SUBSYS=CALL -x NCCL_DEBUG=TRACE -x NCCL_DEBUG_FILE=trace_all_reduce.%h.%p ./build/test/perf/all_reduce_perf -w5 -n5 -b8 -e10M -f2 -d all
-[ $? -ne 0 ] && let failure_count=$failure_count+1 && failure_names+=("Generate all_reduce trace file:\n mpirun $MPI_PARAMS -x NCCL_DEBUG_SUBSYS=CALL -x NCCL_DEBUG=TRACE -x NCCL_DEBUG_FILE=trace_all_reduce.%h.%p ./build/test/perf/all_reduce_perf -w5 -n5 -b8 -e10M -f2 -d all")
+if [ $? -ne 0 ]; then
+  let failure_count=$failure_count+1
+  failure_names+=("Generate all_reduce trace file: mpirun $MPI_PARAMS -x NCCL_DEBUG_SUBSYS=CALL -x NCCL_DEBUG=TRACE -x NCCL_DEBUG_FILE=trace_all_reduce.%h.%p ./build/test/perf/all_reduce_perf -w5 -n5 -b8 -e10M -f2 -d all")
+fi  
 cat trace_all_reduce* > full_trace_all_reduce.txt
 rm trace_all_reduce*
 
 echo "=============================== Generating alltoall trace file - $(date +\"%T\") ================================="
 $SALLOC_SELF_MULTI $MPI_HOME/bin/mpirun $MPI_PARAMS -x NCCL_DEBUG_SUBSYS=CALL -x NCCL_DEBUG=TRACE -x NCCL_DEBUG_FILE=trace_alltoall.%h.%p ./build/test/perf/alltoall_perf -w5 -n5 -b1K -e10M -f2
-[ $? -ne 0 ] && let failure_count=$failure_count+1 && failure_names+=("Generate alltoall trace file:\n mpirun $MPI_PARAMS -x NCCL_DEBUG_SUBSYS=CALL -x NCCL_DEBUG=TRACE -x NCCL_DEBUG_FILE=trace_alltoall.%h.%p ./build/test/perf/alltoall_perf -w5 -n5 -b1K -e10M -f2")
+if [ $? -ne 0 ]; then
+  let failure_count=$failure_count+1
+  failure_names+=("Generate alltoall trace file: mpirun $MPI_PARAMS -x NCCL_DEBUG_SUBSYS=CALL -x NCCL_DEBUG=TRACE -x NCCL_DEBUG_FILE=trace_alltoall.%h.%p ./build/test/perf/alltoall_perf -w5 -n5 -b1K -e10M -f2")
+fi
 cat trace_alltoall* > full_trace_alltoall.txt
 rm trace_alltoall*
 
@@ -33,7 +39,7 @@ $SALLOC_SELF_MULTI $MPI_HOME/bin/mpirun $MPI_PARAMS ./build/test/replay/replay -
 if [ $? -ne 0 ]; then
   let failure_count=$failure_count+1
   failure_names+=("Replay full_trace_all_reduce:")
-  failure_names+=("Generate alltoall trace file : mpirun $MPI_PARAMS -x NCCL_DEBUG_SUBSYS=CALL -x NCCL_DEBUG=TRACE -x NCCL_DEBUG_FILE=trace_all_reduce.%h.%p ./build/test/perf/all_reduce_perf -w5 -n5 -b8 -e10M -f2 -d all && cat trace_all_reduce* >> full_trace_alltoall.txt")
+  failure_names+=("Generate all_reduce trace file : mpirun $MPI_PARAMS -x NCCL_DEBUG_SUBSYS=CALL -x NCCL_DEBUG=TRACE -x NCCL_DEBUG_FILE=trace_all_reduce.%h.%p ./build/test/perf/all_reduce_perf -w5 -n5 -b8 -e10M -f2 -d all && cat trace_all_reduce* >> full_trace_all_reduce.txt")
   failure_names+=("Replay full_trace_all_reduce : mpirun $MPI_PARAMS ./build/test/replay/replay -b full_trace_all_reduce.txt")
 fi
 
@@ -42,7 +48,7 @@ $SALLOC_SELF_MULTI $MPI_HOME/bin/mpirun $MPI_PARAMS ./build/test/replay/replay -
 if [ $? -ne 0 ]; then
   let failure_count=$failure_count+1
   failure_names+=("Replay full_trace_alltoall:")
-  failure_names+=("Generate alltoall trace file : mpirun $MPI_PARAMS -x NCCL_DEBUG_SUBSYS=CALL -x NCCL_DEBUG=TRACE -x NCCL_DEBUG_FILE=trace_alltoall.%h.%p ./build/test/perf/alltoall_perf -w5 -n5 -b1K -e10M -f2 && cat trace_all_reduce* >> full_trace_alltoall.txt")
+  failure_names+=("Generate alltoall trace file : mpirun $MPI_PARAMS -x NCCL_DEBUG_SUBSYS=CALL -x NCCL_DEBUG=TRACE -x NCCL_DEBUG_FILE=trace_alltoall.%h.%p ./build/test/perf/alltoall_perf -w5 -n5 -b1K -e10M -f2 && cat trace_alltoall* >> full_trace_alltoall.txt")
   failure_names+=("Replay full_trace_alltoall : mpirun $MPI_PARAMS ./build/test/replay/replay -b full_trace_alltoall.txt")
 fi
 
@@ -57,7 +63,7 @@ fi
 
 for str in "${failure_names[@]}"
 do
-  printf "Failed Step: $str"
+  echo "Failed Step: $str"
 done
 
 echo "$failure_count tests failed"
