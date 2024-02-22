bbfe99451e3182a94524137a2b98ae80f3e9873e
Sylvain Jeaugey <sjeaugey@nvidia.com>
2024-02-22 06:06:01 -0800

    Make Thread Role organization dependent on the max send/recv peers

--- src/device/prims_simple.h
+++ src/device/prims_simple.h
@@ -528,7 +528,10 @@ private:
     while (nsend < MaxSend && sendPeers[nsend] != -1) nsend++;
     this->fan = Fan(nrecv, nsend);
 
-    constexpr int ThreadPerSync = 32;
+    constexpr int ThreadPerSync =
+      MaxSend >= 16 || MaxRecv >= 16 ? 32 : // NVLS may have an arity > 8. In that case increase the size of the groups
+      MaxSend >= 8 || MaxRecv >= 8 ? 16 :
+      8; // Allows for all roles (WaitRecv/WaitSend/PostRecv/PostSend) within a single warp
     static_assert(MaxSend <= ThreadPerSync && MaxRecv <= ThreadPerSync, "Not enough threads to cover all peers");
 
     int g = tid / ThreadPerSync;
