630fba198248721c92bf2b6fd39a867cfe49af35
Ben Williams <bewilliams@nvidia.com>
2024-02-26 09:27:48 -0800

    Double channels past 1 node instead of 4

--- src/graph/connect.cc
+++ src/graph/connect.cc
@@ -459,9 +459,9 @@ ncclResult_t ncclTopoPostset(struct ncclComm* comm, int* firstRanks, int* treePa
      nChannels = comm->nChannels = copyChannels(comm, nChannels, 2*nChannels, ringPrev, ringNext);
   }
 
-  // Double the number of channels when using unpack beyond 4 nodes.
+  // Double the number of channels when using unpack networking (greater than 1 node)
   // We won't automatically double past 16 channels, users can specify 32 if they want
-  if (comm->netDeviceType == NCCL_NET_DEVICE_UNPACK && comm->nNodes > 4 && nChannels < 16 && ncclParamUnpackDoubleNChannels()) {
+  if (comm->netDeviceType == NCCL_NET_DEVICE_UNPACK && comm->nNodes > 1 && nChannels < 16 && ncclParamUnpackDoubleNChannels()) {
      nChannels = comm->nChannels = copyChannels(comm, nChannels, 2*nChannels, ringPrev, ringNext);
   }
 
