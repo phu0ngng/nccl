ef0da292e474c588b7ace4b10fba597055b65838
Kamil Iskra <kiskra@nvidia.com>
2024-03-11 11:10:33 -0700

    Make test/verifiable a first-class build citizen
    Avoid building libverifiable.so as part of building the perf and replay
    directories, as that can lead to duplicate compilations, race conditions,
    and occasionally object file corruption.  Make verifiable an explicit
    dependency of perf and replay to ensure correct build ordering.

--- test/verifiable/verifiable.mk
+++ test/verifiable/verifiable.mk
@@ -4,14 +4,3 @@
 
 TEST_VERIFIABLE_HDRS = $(TEST_VERIFIABLE_SRCDIR)/verifiable.h
 TEST_VERIFIABLE_LIBSO = $(TEST_VERIFIABLE_BUILDDIR)/libverifiable.so
-
-$(TEST_VERIFIABLE_BUILDDIR)/verifiable.o: $(TEST_VERIFIABLE_SRCDIR)/verifiable.cu $(TEST_VERIFIABLE_HDRS)
-	@printf "Compiling %s\n" $@
-	@mkdir -p $(TEST_VERIFIABLE_BUILDDIR)
-	$(NVCC) -Xcompiler -fPIC -o $@ $(NVCUFLAGS) -c $(TEST_VERIFIABLE_SRCDIR)/verifiable.cu
-
-$(TEST_VERIFIABLE_LIBSO): $(TEST_VERIFIABLE_BUILDDIR)/verifiable.o
-	@printf "Creating %s\n" $@
-	@mkdir -p $(TEST_VERIFIABLE_BUILDDIR)
-	$(CXX) -shared -o $@.0 $^ -Wl,-soname,$(notdir $@).0
-	ln -sf $(notdir $@).0 $@
