2ee8480aa4c7b4d2eaa54d96545ef6414c26b1bc
David Addison <daddison@nvidia.com>
2024-05-07 09:28:14 -0700

    Don't report P2P connect timing unless elapsed > 1.0 Always log timings but add rank to each INFO log

--- src/init.cc
+++ src/init.cc
@@ -1427,7 +1427,7 @@ static ncclResult_t ncclCommInitRankFunc(struct ncclAsyncJob* job_) {
     INFO(NCCL_INIT,"ncclCommInitRank comm %p rank %d nranks %d cudaDev %d nvmlDev %d busId %lx commId 0x%llx - Init COMPLETE",
     comm, comm->rank, comm->nRanks, comm->cudaDev, comm->nvmlDev, comm->busId, (unsigned long long)hashUniqueId(job->commId));
   }
-  if (comm->rank == 0) INFO(NCCL_INIT|NCCL_PROFILE,"Init timings: nranks %d total %.2f (kernels %.2f, bootstrap %.2f, allgathers %.2f, topo %.2f, graphs %.2f, connections %.2f, rest %.2f)\n", comm->nRanks, timers[TIMER_INIT_TOTAL]/1e9,
+  INFO(NCCL_INIT|NCCL_PROFILE,"Init timings: rank %d nranks %d total %.2f (kernels %.2f, bootstrap %.2f, allgathers %.2f, topo %.2f, graphs %.2f, connections %.2f, rest %.2f)\n", comm->rank, comm->nRanks, timers[TIMER_INIT_TOTAL]/1e9,
     timers[TIMER_INIT_KERNELS]/1e9, timers[TIMER_INIT_BOOTSTRAP]/1e9, timers[TIMER_INIT_ALLGATHER]/1e9, timers[TIMER_INIT_TOPO]/1e9, timers[TIMER_INIT_GRAPHS]/1e9, timers[TIMER_INIT_CONNECT]/1e9,
     (timers[TIMER_INIT_TOTAL]-timers[TIMER_INIT_KERNELS]-timers[TIMER_INIT_BOOTSTRAP]-timers[TIMER_INIT_ALLGATHER]-timers[TIMER_INIT_TOPO]-timers[TIMER_INIT_GRAPHS]-timers[TIMER_INIT_CONNECT])/1e9);
 exit:
--- src/transport.cc
+++ src/transport.cc
@@ -220,7 +220,7 @@ ncclResult_t ncclTransportP2pSetup(struct ncclComm* comm, struct ncclTopoGraph* 
     struct timeval now;
     gettimeofday(&now, NULL);
     float elapsed = (now.tv_sec - timeStart.tv_sec)*1.0 + (now.tv_usec-timeStart.tv_usec)*1e-6;
-    if (comm->rank == 0) INFO(NCCL_PROFILE, "timings: nranks %d P2p connect done in %d:%02d", comm->nRanks, ((int)elapsed)/60, ((int)elapsed)%60);
+    if (elapsed > 1.0) INFO(NCCL_PROFILE, "timings: rank %d nranks %d P2p connect done in %.2f", comm->rank, comm->nRanks, elapsed);
     if (timeReported) {
       printf("\rP2p connect done in %d:%02d                                                                       \n",
              ((int)elapsed)/60, ((int)elapsed)%60);
