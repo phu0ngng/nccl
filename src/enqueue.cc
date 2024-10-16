d5bb6b06521ba36bc4fe9865a1c6161700a5b3cf
Ben Williams <bewilliams@nvidia.com>
2024-10-16 11:46:21 -0700

    Enable default alignments of less than 16 bytes

--- src/enqueue.cc
+++ src/enqueue.cc
@@ -1092,8 +1092,8 @@ static ncclResult_t uploadWork(struct ncclComm* comm, struct ncclKernelPlan* pla
     plan->kernelArgs->workBuf = comm->workFifoBufDev;
     break;
   case ncclDevWorkStorageTypePersistent:
-    static_assert(16 <= alignof(max_align_t), "We rely on 16-byte alignment.");
-    fifoBufHost = malloc(workBytes);
+    // We rely on 16-byte alignment
+    fifoBufHost = aligned_alloc(16, ROUNDUP(workBytes, 16));
     fifoCursor = 0;
     fifoMask = ~0u;
     break;
