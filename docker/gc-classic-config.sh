4c5c66fbbaaf1d8b0d1e4dc4217324274dc3dfef
Ben Williams <bewilliams@nvidia.com>
2024-10-16 09:12:41 -0700

    Revert "Building TRACE=1 for gpu-comms builds"
    This reverts commit bcde8434f03febe0b69f3a83d659a44f9648986b.

--- docker/gc-classic-config.sh
+++ docker/gc-classic-config.sh
@@ -71,7 +71,7 @@ function get_build_command() {
         -e DOCKER_GROUP_ID \
         -v ${current_dir}:/nccl \
         $build_tools_image \
-        /nccl/docker/build_nccl.sh TRACE=1"
+        /nccl/docker/build_nccl.sh"
 }
 
 function get_cuda_home() {
