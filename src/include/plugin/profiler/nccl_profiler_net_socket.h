9c9c997d6230d93a17c9c8bdb43b12da2e47df10
Giuseppe Congiu <gcongiu@nvidia.com>
2024-10-25 06:23:51 -0700

    Net/SOCK: instrument plugin with profiler
    Instrument test function. isend/irecv in the case of socket plugin are
    only invoked to allocate a request object. Real network progress starts
    only after a call to the test function. In this case there are two
    possibilities: main thread drives progress (non overlapped), the plugin
    creates a number of task objects, spawns a set of threads (each handling
    a number of sockets) - AWS uses 8 threads and 2 sockets per thread,
    Google uses 4 threads and 1 socket per thread - and assigns tasks to
    threads for execution. Both cases are instrumented with the NCCL core
    profiler callback.

---
+++ src/include/plugin/profiler/nccl_profiler_net_socket.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+ * Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
+ *
+ * See LICENSE.txt for license information
+ ************************************************************************/
+
+#ifndef NCCL_PROFILER_NET_SOCKET_H_
+#define NCCL_PROFILER_NET_SOCKET_H_
+
+#include "nccl_profiler.h"
+#include "nccl_profiler_net_socket_v1.h"
+
+#endif
