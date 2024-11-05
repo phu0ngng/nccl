6bdbf7d258ae691c1dbc9756c9d4ffb803d82f26
Kaiming Ouyang <kouyang@nvidia.com>
2024-11-05 09:36:51 -0800

    Disable net buffer registration on GCP

--- src/register/coll_reg.cc
+++ src/register/coll_reg.cc
@@ -327,7 +327,7 @@ ncclResult_t ncclRegisterCollBuffers(
 
       // start net registration
       regBufFlag = 0;
-      if (!comm->useNetPXN && comm->useGdr) {
+      if (!comm->useNetPXN && comm->useGdr && comm->netDeviceType != NCCL_NET_DEVICE_UNPACK) {
         if (comm->planner.persistent && ncclParamGraphRegister()) {
           if (hasSendNetPeer) {
             ncclNetGraphRegisterBuffer(comm, info->sendbuff, sendbuffSize, sendNetConns, sendNetPeers, &regBufFlag, sendNetHandles, cleanupQueue, &info->nCleanupQueueElts);
