5624fba08e5c7a8a33cd3927b519be7463c111a8
Ben Williams <bewilliams@nvidia.com>
2025-03-13 14:28:24 -0700

    Updated pytest k-expr to filter for tests

--- test/scripts/dlfw/nccl_pytorch_distributed_test.sh
+++ test/scripts/dlfw/nccl_pytorch_distributed_test.sh
@@ -53,4 +53,5 @@ export PYTORCH_TEST_RUN_EVERYTHING_IN_SERIAL=1
 pip install --upgrade --force-reinstall zstandard
 pip install --upgrade --force-reinstall zstd
 export NCCL_DEBUG=WARN
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python run_test.py --shard ${shard_id} ${nshards} --distributed-tests -v --pipe-logs -x ${EXCLUDE_PYTEST[@]} --continue-through-error -- -k "nccl"
+export PYTEST_K_EXPR="nccl and not test_close_multi_pg_unordered"
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python run_test.py --shard ${shard_id} ${nshards} --distributed-tests -v --pipe-logs -x ${EXCLUDE_PYTEST[@]} --continue-through-error -- -k "$PYTEST_K_EXPR"
