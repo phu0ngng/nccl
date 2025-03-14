321ad5428469f14da24a43a60ef5977bc912cd8c
Ben Williams <bewilliams@nvidia.com>
2025-03-14 06:53:34 -0700

    Adding k-expr to test case

--- test/scripts/dlfw/nccl_pytorch_distributed_test.sh
+++ test/scripts/dlfw/nccl_pytorch_distributed_test.sh
@@ -53,5 +53,5 @@ export PYTORCH_TEST_RUN_EVERYTHING_IN_SERIAL=1
 pip install --upgrade --force-reinstall zstandard
 pip install --upgrade --force-reinstall zstd
 export NCCL_DEBUG=WARN
-export PYTEST_K_EXPR="nccl and not test_close_multi_pg_unordered"
+export PYTEST_K_EXPR="nccl and not (test_close_multi_pg_unordered or test_intra_node_comm_all_reduce)"
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python run_test.py --shard ${shard_id} ${nshards} --distributed-tests -v --pipe-logs -x ${EXCLUDE_PYTEST[@]} --continue-through-error -- -k "$PYTEST_K_EXPR"
