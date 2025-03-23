3d24b97e74d56a087b9ee98b4fa8be6daaaca47a
Sreeram Potluri <spotluri@prenyx0073.a51.clusters.nvidia.com>
2025-03-23 09:25:20 -0700

    avoid duplicate and unused QP for PXN
    check for accept listen and accept to avoid new connections that
    are never completed
    
    See merge request nccl/nccl!851

--- src/include/proxy.h
+++ src/include/proxy.h
@@ -226,6 +226,8 @@ struct ncclProxyPeer {
 };
 
 struct ncclSharedNetComms {
+  int activeConnect[MAXCHANNELS];
+  int activeAccept[MAXCHANNELS];
   void* sendComm[MAXCHANNELS];
   void* recvComm[MAXCHANNELS];
   int sendRefCount[MAXCHANNELS];
