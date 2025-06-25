ff6db12f324ecb766cf6917ec3861577f66bc970
Giuseppe Congiu <gcongiu@nvidia.com>
2025-06-25 07:31:33 -0700

    tuner: fix memory leak

--- src/plugin/tuner.cc
+++ src/plugin/tuner.cc
@@ -82,6 +82,7 @@ exit:
   pthread_mutex_unlock(&tunerPluginLock);
   return ncclSuccess;
 fail:
+  if (tunerPluginLib) NCCLCHECK(ncclClosePluginLib(tunerPluginLib, ncclPluginTypeTuner));
   tunerPluginLib = nullptr;
   status = tunerPluginLoadFailed;
   goto exit;
