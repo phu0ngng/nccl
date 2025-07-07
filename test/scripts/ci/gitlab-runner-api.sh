1a4424df59cd6bbd3e65f4d88fde2d54ba7f6b7f
Giuseppe Congiu <gcongiu@nvidia.com>
2025-07-07 10:41:55 -0700

    Fix CI for API tests
    BUG: 5370879

--- test/scripts/ci/gitlab-runner-api.sh
+++ test/scripts/ci/gitlab-runner-api.sh
@@ -38,6 +38,7 @@ run_api_test(){
 
 # list of tests with a special config
 multinetTests="ncclCommInitRankConfig_test.multi_net_plugin_*"
+sharedPluginTest="ncclCommInitRankConfig_test.shared_plugin_lib"
 
 # run all tests except the ones with a special config
 run_api_test "" "-${multinetTests}:${sharedPluginTest}"
@@ -47,7 +48,6 @@ export NCCL_NET_PLUGIN="plugin_nodev_v5,plugin_nodev_v6,plugin_v7,plugin_nodev_v
 run_api_test "multinet_" "${multinetTests}"
 unset NCCL_NET_PLUGIN
 
-sharedPluginTest="ncclCommInitRankConfig_test.shared_plugin_lib"
 export NCCL_NET_PLUGIN="libnccl-shared-plugins.so"
 run_api_test "" "${sharedPluginTest}"
 unset NCCL_NET_PLUGIN
