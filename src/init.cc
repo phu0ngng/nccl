7ad58110285896135b5f923ac7b50a2034c60c4b
Kamil Iskra <kiskra@nvidia.com>
2025-08-01 21:26:59 -0700

    Squash commit for Fix stack overflow issues on MNNVL systems
    Some (misconfigured!) systems come with a default stack size limit of
    "unlimited".  Due to an idiosyncracy of GNU libc (see pthread_create(3)),
    this actually results in a _decrease_ of the default pthread stack size to
    just 2 MB (!), which is insufficient for the graph search above some 50
    MNNVL-connected ranks.
    
    Query the current setting and, if needed, issue diagnostics and change the
    default using a GNU-libc-specific API.  We don't want to explicitly pass
    the attributes to pthread_create because that option won't be available if
    we switch to C++ threads.
    
    BUG 5309620
    
    BUG 5356271
    
    BUG 5420198
    
    BUG 5381348
    
    Fix stack overflow issues on MNNVL systems
    
    Some (misconfigured!) systems come with a default stack size limit of
    "unlimited".  Due to an idiosyncracy of GNU libc (see pthread_create(3)),
    this actually results in a _decrease_ of the default pthread stack size to
    just 2 MB (!), which is insufficient for the graph search above some 50
    MNNVL-connected ranks.
    
    Query the current setting and, if needed, issue diagnostics and change the
    default using a GNU-libc-specific API.  We don't want to explicitly pass
    the attributes to pthread_create because that option won't be available if
    we switch to C++ threads.
    
    BUG 5309620
    BUG 5356271
    BUG 5420198
    
    See merge request nccl/nccl!1158

--- src/init.cc
+++ src/init.cc
@@ -27,6 +27,7 @@
 #include <dlfcn.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/resource.h>
 #include <unistd.h>
 #include "param.h"
 #include "nvtx_payload_schemas.h"
@@ -57,6 +58,7 @@ NCCL_PARAM(WinEnable, "WIN_ENABLE", 1);
 NCCL_PARAM(CollnetEnable, "COLLNET_ENABLE", NCCL_CONFIG_UNDEF_INT);
 NCCL_PARAM(CtaPolicy, "CTA_POLICY", NCCL_CONFIG_UNDEF_INT);
 NCCL_PARAM(NvlsChannels, "NVLS_NCHANNELS", NCCL_CONFIG_UNDEF_INT);
+NCCL_PARAM(SetCpuStackSize, "SET_CPU_STACK_SIZE", 1);
 
 extern int64_t ncclParamSingleProcMemRegEnable();
 
@@ -75,11 +77,50 @@ ncclResult_t initGdrCopy() {
   return ncclSuccess;
 }
 
+// The default Linux stack size (8MB) is safe.
+#define SAFE_STACK_SIZE (8192*1024)
+
+static ncclResult_t setCpuStackSize() {
+  if (ncclParamSetCpuStackSize() != 0) {
+    // Query the stack size used for newly launched threads.
+    pthread_attr_t attr;
+    size_t stackSize;
+    PTHREADCHECK(pthread_attr_init(&attr), "pthread_attr_init");
+    PTHREADCHECK(pthread_attr_getstacksize(&attr, &stackSize), "pthread_attr_getstacksize");
+
+    if (stackSize < SAFE_STACK_SIZE) {
+      // GNU libc normally uses RLIMIT_STACK as the default pthread stack size, unless it's set to "unlimited" --
+      // in that case a fallback value of 2MB (!) is used.
+
+      // Query the actual resource limit so that we can distinguish between the settings of 2MB and unlimited.
+      struct rlimit stackLimit;
+      char buf[30];
+      SYSCHECK(getrlimit(RLIMIT_STACK, &stackLimit), "getrlimit");
+      if (stackLimit.rlim_cur == RLIM_INFINITY)
+        strcpy(buf, "unlimited");
+      else
+        snprintf(buf, sizeof(buf), "%ldKB", stackLimit.rlim_cur/1024);
+      INFO(NCCL_INIT|NCCL_ENV, "Stack size limit (%s) is unsafe; will use %dKB for newly launched threads",
+           buf, SAFE_STACK_SIZE/1024);
+
+      // Change the default pthread stack size (via a nonportable API, which will become necessary if we switch
+      // to C++ threads).
+      PTHREADCHECK(pthread_attr_setstacksize(&attr, SAFE_STACK_SIZE), "pthread_attr_setstacksize");
+      PTHREADCHECK(pthread_setattr_default_np(&attr), "pthread_setattr_default_np");
+    }
+
+    PTHREADCHECK(pthread_attr_destroy(&attr), "pthread_attr_destroy");
+  }
+
+  return ncclSuccess;
+}
+
 static ncclResult_t initResult = ncclSuccess;
 static std::once_flag initOnceFlag;
 
 static void initOnceFunc() {
   initEnv();
+  setCpuStackSize();
   initGdrCopy();
   // Always initialize bootstrap network
   NCCLCHECKGOTO(bootstrapNetInit(), initResult, exit);
