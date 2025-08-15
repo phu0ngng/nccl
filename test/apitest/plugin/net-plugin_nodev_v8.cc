1e47b881529798b6162af0030180df09b665ec47
Giuseppe Congiu <gcongiu@nvidia.com>
2025-08-15 09:27:31 -0700

    test: add missing ncclNet_v8 sym to api tests plugin
    In 'test/api/plugin/net-plugin_nodev_v8.cc', the plugin symbol table was
    missing a function pointer to 'getDeviceMr'. This caused a mismatch
    between the expected plugin symbol struct and the provided plugin symbol
    struct. On some QA systems this showed up as build time error:
    
    """
    sorry, unimplemented: non-trivial designated initializers not supported
    """

--- test/apitest/plugin/net-plugin_nodev_v8.cc
+++ test/apitest/plugin/net-plugin_nodev_v8.cc
@@ -25,6 +25,7 @@ __hidden ncclResult_t pluginCloseSend(void* sendComm) { return ncclInternalError
 __hidden ncclResult_t pluginCloseRecv(void* recvComm) { return ncclInternalError; }
 __hidden ncclResult_t pluginCloseListen(void* listenComm) { return ncclInternalError; }
 __hidden ncclResult_t pluginIrecvConsumed(void* recvComm, int n, void* request) { return ncclInternalError; }
+__hidden ncclResult_t pluginGetDeviceMr(void* comm, void* mhandle, void** dptr_mhandle) { return ncclInternalError; }
 
 extern "C" __attribute__((visibility("default"))) const ncclNet_v8_t ncclNetPlugin_v8 = {
   .name = "ncclNetPlugin_v8",
@@ -44,5 +45,6 @@ extern "C" __attribute__((visibility("default"))) const ncclNet_v8_t ncclNetPlug
   .closeSend = pluginCloseSend,
   .closeRecv = pluginCloseRecv,
   .closeListen = pluginCloseListen,
+  .getDeviceMr = pluginGetDeviceMr,
   .irecvConsumed = pluginIrecvConsumed,
 };
