a8633fcd9d24b363e6e28062701e9006ef406bdc
Kamil Iskra <kiskra@nvidia.com>
2025-09-03 16:30:59 -0700

    Exclude symmetric support from NCCL_P2P_LEVEL
    Add a new isAllCudaP2p member to struct ncclComm, indicating raw CUDA
    P2P capability between local ranks.  Use that (instead of
    isAllDirectP2p) to determine symmetricSupport.
    
    In the process also tweak isAllDirectP2p to be true for communicators
    with 1 GPU/node, as it seems more logical that way.

--- src/include/comm.h
+++ src/include/comm.h
@@ -471,7 +471,7 @@ struct ncclComm {
   int maxTreePattern;
   bool initAlgoChannels[NCCL_NUM_ALGORITHMS];
   bool runtimeConn; // if dynamic connection is supported
-  bool directMode;
+  bool directMode; // if any process manages more than one local rank
   int cuMemSupport;
 
   uint64_t magic; // Magic number for all network communication. Not a security key -- only goal is to detect mismatches.
@@ -659,7 +659,8 @@ struct ncclComm {
   // buffer registration cache
   struct ncclRegCache regCache;
   int isAllNvlink;
-  bool isAllDirectP2p;
+  bool isAllDirectP2p; // Subject to NCCL_P2P_LEVEL (for local ranks only).
+  bool isAllCudaP2p; // Raw CUDA capability (for local ranks only).
   int symmetricSupport;
   bool useNetPXN;
   bool useGdr;
