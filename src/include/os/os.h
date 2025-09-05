9f545dc678d5dd9e6d52f4c8d3e2aee7c8b03be9
Aurelien Chartier <achartier@nvidia.com>
2025-09-05 19:38:38 +0000

    Move CPU stack size override to os layer

--- src/include/os/os.h
+++ src/include/os/os.h
@@ -1,1 +1,18 @@
+/*************************************************************************
+ * Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+ *
+ * See LICENSE.txt for license information
+ ************************************************************************/
+
+ #ifndef NCCL_OS_H_
+ #define NCCL_OS_H_
+
+#include "nccl.h"
+
+#include <cstdint>
+
 uint64_t ncclOsGetpid();
+
+ncclResult_t ncclOsSetCpuStackSize();
+
+#endif
--- src/os/windows.cc
+++ src/os/windows.cc
@@ -1,5 +1,16 @@
-#include <cstdint>
+/*************************************************************************
+ * Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+ *
+ * See LICENSE.txt for license information
+ ************************************************************************/
+
+#include "os/os.h"
 
 uint64_t ncclOsGetpid() {
     return (uint64_t)GetCurrentProcessId();
+}
+
+ncclResult_t ncclOsSetCpuStackSize() {
+    printf("ncclOsSetCpuStackSize: Not implemented on Windows\n");
+    return ncclSuccess;
 }
