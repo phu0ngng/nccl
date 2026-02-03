eb0a6682ced2854d6ec971ff4197ce63e54c954f
Katie Gioioso <kgioioso@nvidia.com>
2026-02-03 23:08:12 +0000

    move device api test to seprarte function

--- test/scripts/ci/gitlab-runner-api.sh
+++ test/scripts/ci/gitlab-runner-api.sh
@@ -34,12 +34,14 @@ run_api_test(){
     else
       echo -e "Disabled Api TESTS network test\n\n"
     fi
+}
 
-    if [[ ${API_TESTS_DEVICE_API} -eq 1 && -z "${NCCL_NET_PLUGIN:-}" ]] ; then
-        run_command "device_apitest_${1}gin_cpu_proxy" "$RUN_MODE" 1 "--oversubscribe" "NCCL_GIN_TYPE=2" "$NCCL_HOME/test/apitest/device_api/device_api_test --gtest_filter=${2}" ""
-        run_command "device_apitest_${1}gin_gdaki_sm" "$RUN_MODE" 1 "--oversubscribe" "NCCL_GIN_TYPE=3 NCCL_GIN_GDAKI_NIC_HANDLER=2" "$NCCL_HOME/test/apitest/device_api/device_api_test --gtest_filter=${2}" ""
-        # TODO: Remove filter when https://nvbugspro.nvidia.com/bug/5866950 is fixed.
-        run_command "device_apitest_${1}gin_gdaki_cpu_assisted" "$RUN_MODE" 1 "--oversubscribe" "NCCL_GIN_TYPE=3 NCCL_GIN_GDAKI_NIC_HANDLER=1" "$NCCL_HOME/test/apitest/device_api/device_api_test --gtest_filter=-*context_1*" ""
+run_device_api_test(){
+    if [[ ${API_TESTS_DEVICE_API} -eq 1 ]] ; then
+      run_command "device_apitest_gin_cpu_proxy" "$RUN_MODE" 1 "--oversubscribe" "NCCL_GIN_TYPE=2" "$NCCL_HOME/test/apitest/device_api/device_api_test" ""
+      run_command "device_apitest_gin_gdaki_sm" "$RUN_MODE" 1 "--oversubscribe" "NCCL_GIN_TYPE=3 NCCL_GIN_GDAKI_NIC_HANDLER=2" "$NCCL_HOME/test/apitest/device_api/device_api_test" ""
+      # TODO: Remove filter when https://nvbugspro.nvidia.com/bug/5866950 is fixed.
+      run_command "device_apitest_gin_gdaki_cpu_assisted" "$RUN_MODE" 1 "--oversubscribe" "NCCL_GIN_TYPE=3 NCCL_GIN_GDAKI_NIC_HANDLER=1" "$NCCL_HOME/test/apitest/device_api/device_api_test --gtest_filter=-*context_1*" ""
     else
       echo -e "Disabled Api TESTS device api test\n\n"
     fi 
@@ -56,6 +58,8 @@ fi
 
 # run all tests except the ones with a special config
 run_api_test "" "${gtestFilter}"
+
+run_device_api_test
 
 # run multinet tests with special config
 export NCCL_NET_PLUGIN="plugin_nodev_v6,plugin_v7,plugin_nodev_v8,plugin_nodev_v9,plugin_nodev_v10,plugin_nodev_v11"
