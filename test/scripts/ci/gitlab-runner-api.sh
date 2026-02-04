ed228f7f76482e2252d61413e49177bf3ef260af
Katie Gioioso <kgioioso@nvidia.com>
2026-02-04 22:28:50 +0000

    remove filter now that gdaki+cpu is fixed

--- test/scripts/ci/gitlab-runner-api.sh
+++ test/scripts/ci/gitlab-runner-api.sh
@@ -40,8 +40,7 @@ run_device_api_test(){
     if [[ ${API_TESTS_DEVICE_API} -eq 1 ]] ; then
       run_command "device_apitest_gin_cpu_proxy" "$RUN_MODE" 1 "--oversubscribe" "NCCL_GIN_TYPE=2" "$NCCL_HOME/test/apitest/device_api/device_api_test" ""
       run_command "device_apitest_gin_gdaki_sm" "$RUN_MODE" 1 "--oversubscribe" "NCCL_GIN_TYPE=3 NCCL_GIN_GDAKI_NIC_HANDLER=2" "$NCCL_HOME/test/apitest/device_api/device_api_test" ""
-      # TODO: Remove filter when https://nvbugspro.nvidia.com/bug/5866950 is fixed.
-      run_command "device_apitest_gin_gdaki_cpu_assisted" "$RUN_MODE" 1 "--oversubscribe" "NCCL_GIN_TYPE=3 NCCL_GIN_GDAKI_NIC_HANDLER=1" "$NCCL_HOME/test/apitest/device_api/device_api_test --gtest_filter=-*context_1*" ""
+      run_command "device_apitest_gin_gdaki_cpu_assisted" "$RUN_MODE" 1 "--oversubscribe" "NCCL_GIN_TYPE=3 NCCL_GIN_GDAKI_NIC_HANDLER=1" "$NCCL_HOME/test/apitest/device_api/device_api_test" ""
     else
       echo -e "Disabled Api TESTS device api test\n\n"
     fi 
