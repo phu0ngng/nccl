743aa838ff85ea07c9890668e6f85ec59cfb346e
Kamil Iskra <kiskra@nvidia.com>
2026-02-09 16:35:58 -0800

    Improve GIN handling ncclCommGetAsyncError
    Fix a race condition during GIN initialization -- if
    ncclCommGetAsyncError is invoked while GDAKI is still initializing,
    spurious errors could be returned.  Delay GIN checks until
    ginState.connected is true.
    
    Also remove a redundant loop over the available connections around the
    invocation of ncclGinQueryLastError.  That function performs such a loop
    internally.
    
    https://github.com/NVIDIA/nccl/issues/2019

--- src/init.cc
+++ src/init.cc
@@ -3089,20 +3089,17 @@ ncclResult_t ncclCommGetAsyncError(ncclComm_t comm, ncclResult_t *asyncError) {
   if (*asyncError == ncclSuccess && comm->proxyState) *asyncError = COMPILER_ATOMIC_LOAD(&comm->proxyState->asyncResult, std::memory_order_acquire);
 
   /* Check gin status */
-  if (*asyncError == ncclSuccess && comm->sharedRes && comm->sharedRes->ginState.ncclGin) {
+  if (*asyncError == ncclSuccess && comm->sharedRes && comm->sharedRes->ginState.connected) {
     struct ncclGinState* ginState = &comm->sharedRes->ginState;
     // Gin progress thread status
     if (ginState->needsProxyProgress) *asyncError = COMPILER_ATOMIC_LOAD(&comm->sharedRes->ginState.asyncResult, std::memory_order_acquire);
     // Gin side errors, also works when we have no GIN progress thread.
     if (*asyncError == ncclSuccess) {
       bool ginError;
-      for (int c=0; c<comm->sharedRes->ginState.ginCommCount; c++) {
-        NCCLCHECK(ncclGinQueryLastError(&comm->sharedRes->ginState, &ginError));
-        if (ginError) {
-          WARN("GIN Error on gin context %d\n", c);
-          *asyncError = ncclRemoteError;
-          break;
-        }
+      NCCLCHECK(ncclGinQueryLastError(&comm->sharedRes->ginState, &ginError));
+      if (ginError) {
+        WARN("GIN Error detected");
+        *asyncError = ncclRemoteError;
       }
     }
   }
