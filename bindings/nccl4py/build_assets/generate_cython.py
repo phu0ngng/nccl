25d8df48c2de12411ad4fb02627da8743941fd61
Jay Skrobola <jskrobola@nvidia.com>
2026-02-19 08:09:06 -0600

    Update to Apache 2.0 License Pt 3
    Covers another set of files that were either missing copyright headers or still using the old format.
    Updated src/include/tuner.h to use the dual Apache-2.0/BSD-3 header

--- bindings/nccl4py/build_assets/generate_cython.py
+++ bindings/nccl4py/build_assets/generate_cython.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
 #
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto.  Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 #
-# See LICENSE.txt for license information
+# See LICENSE.txt for more license information
+#
 
 """
 Generate Cython bindings for NCCL using cybind.
--- plugins/mixed/example/plugin.c
+++ plugins/mixed/example/plugin.c
@@ -1,9 +1,9 @@
-/*
+/*************************************************************************
  * SPDX-FileCopyrightText: Copyright (c) 2024-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * See LICENSE.txt for more license information
- */
+ *************************************************************************/
 
 #include "net.h"
 
