36b06d68a74e20a470c39b96c5fcbde5db665f76
Jie Zhang <jiezhang@nvidia.com>
2026-03-30 16:49:37 +0800

    Infer glibc cutoff from libnccl.so symbol versions instead of environment

--- test/binary/test_lib.sh
+++ test/binary/test_lib.sh
@@ -19,10 +19,16 @@ stripldd() {
 
 libs="linux-vdso.so libstdc++.so libm.so libgcc_s.so libc.so ld-linux-x86-64.so"
 # Glibc 2.34 now integrates libpthread, librt and libdl. Do not check them on recent distros.
-glibc_version=`ldd --version | head -1 | tr -s ' ' '\n' | tail -1`
-glibc_version_major=`echo $glibc_version | cut -d '.' -f 1`
-glibc_version_minor=`echo $glibc_version | cut -d '.' -f 2`
-if [ "$glibc_version_major" -lt "2" -o "$glibc_version_minor" -lt "34" ]; then
+glibc_version=""
+if command -v objdump >/dev/null 2>&1; then
+  glibc_version=$(objdump -T "$lib" 2>/dev/null | grep -oE 'GLIBC_[0-9][0-9.]*' | sed 's/^GLIBC_//' | sort -Vu | tail -1)
+fi
+if [ -z "$glibc_version" ]; then
+  glibc_version=$(ldd --version | head -1 | tr -s ' ' '\n' | tail -1)
+fi
+glibc_version_major=$(echo "$glibc_version" | cut -d '.' -f 1)
+glibc_version_minor=$(echo "$glibc_version" | cut -d '.' -f 2)
+if [ "$glibc_version_major" -lt "2" ] || [ "$glibc_version_minor" -lt "34" ]; then
   libs="$libs librt.so libdl.so libpthread.so"
 fi
 
