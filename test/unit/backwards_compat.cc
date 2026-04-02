4fda087109eaa4e3a8cea4a35fd91c2694fffcb1
Sylvain Jeaugey <sjeaugey@nvidia.com>
2026-04-02 00:20:28 -0700

    Add ginTrafficClass to ncclDevCommRequirements
    Use the comm traffic class if the devCommRequirement is unset.
    
    In order of priority we have: NCCL_IB_TC/NCCL_IB_SL, devComm
    requirement, comm config.
    
    Create traffic class get/set functions in the main NET IB code
    to group all ctx<->config casts in nearby functions and lower
    the risk of a change breaking GIN Proxy traffic class silently.
    
    Signed-off-by: Sylvain Jeaugey <sjeaugey@nvidia.com>

--- test/unit/backwards_compat.cc
+++ test/unit/backwards_compat.cc
@@ -102,9 +102,11 @@ void ncclDevCommRequirements_backwards_compat_test() {
   static_assert(offsetof(ncclDevCommRequirements_t, ginConnectionType) == 72);
   static_assert(offsetof(ncclDevCommRequirements_t, ginExclusiveContexts) == 76);
   static_assert(offsetof(ncclDevCommRequirements_t, ginQueueDepth) == 80);
+  static_assert(offsetof(ncclDevCommRequirements_t, ginTrafficClass) == 84);
+  static_assert(offsetof(ncclDevCommRequirements_t, worldGinBarrierCount) == 88);
 
   // This check prompts users to update the test. Edit according to the instructions above.
-  static_assert(sizeof(ncclDevCommRequirements_t) == 88);
+  static_assert(sizeof(ncclDevCommRequirements_t) == 96);
 
   static_assert(offsetof(ncclDevResourceRequirements_t, next) == 0);
   static_assert(offsetof(ncclDevResourceRequirements_t, bufferSize) == 8);
